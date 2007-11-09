94ab8692f5c107bfb049ce6277e494e259bfe8c8
Eleftherios Spyromitros-Xioufis <espyromi@csd.auth.gr>
2007-11-09 12:52:55 +0000

    draft
    git-svn-id: svn://svn.code.sf.net/p/mulan/code/trunk@13 90ebec12-01d7-49c3-8feb-d42dc2bd4ece

---
+++ mulan/test/testmlknn.java
@@ -0,0 +1,63 @@
+import java.io.BufferedWriter;
+import java.io.FileReader;
+import java.io.FileWriter;
+
+
+import mulan.Statistics;
+import mulan.classifier.MultiLabelKNN;
+import mulan.evaluation.Evaluation;
+import mulan.evaluation.Evaluator;
+import mulan.evaluation.LabelBasedEvaluation;
+import weka.classifiers.Classifier;
+import weka.classifiers.functions.SMO;
+import weka.classifiers.trees.J48;
+import weka.core.Instances;
+
+public class testmlknn {
+
+	public testmlknn() {
+	}
+
+	public static void main(String[] args) throws Exception {
+		
+		String path = "E:/Documents and Settings/lefman/Desktop/my workspace/datasets/";
+		String datastem = "yeast";
+		int numLabels = 14;
+
+		FileReader frTrain = new FileReader(path + datastem + "-train.arff");
+		Instances trainData = new Instances(frTrain);
+		FileReader frTest = new FileReader(path + datastem + "-test.arff");
+		Instances testData = new Instances(frTest);
+		
+		Classifier mlknn = new MultiLabelKNN(14,2,1);
+		
+		mlknn.buildClassifier(trainData);
+
+		
+		 /*// show multilabel statistics 
+		Instances allData = new Instances(trainData);
+        for (int i=0; i<testData.numInstances(); i++)
+            allData.add(testData.instance(i));
+
+        Statistics stats = new Statistics();
+        stats.calculateStats(allData, numLabels);
+        System.out.println(stats.toString());
+		 
+		 /*  // Binary Relevance Classifier Evaluator eval; Evaluation results;
+		 * System.out.println("BR"); BinaryRelevanceClassifier br = new
+		 * BinaryRelevanceClassifier();
+		 * br.setBaseClassifier(Classifier.makeCopy(baseClassifier));
+		 * br.setNumLabels(numLabels); br.buildClassifier(trainData); eval = new
+		 * Evaluator(); results = eval.evaluate(br, testData);
+		 * results.getLabelBased().setAveragingMethod(LabelBasedEvaluation.MICRO);
+		 * System.out.println("HammingLoss : " +
+		 * results.getExampleBased().hammingLoss());
+		 * System.out.println("Precision : " +
+		 * results.getLabelBased().precision()); System.out.println("Recall : " +
+		 * results.getLabelBased().recall()); System.out.println("F1 : " +
+		 * results.getLabelBased().fmeasure());
+		 * 
+		 */
+
+	}
+}
