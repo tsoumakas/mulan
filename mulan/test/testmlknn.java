242c003efe85a50678954c5ecaf1debc5b98831c
Eleftherios Spyromitros-Xioufis <espyromi@csd.auth.gr>
2007-11-27 19:05:05 +0000

    test for k=1..12
    git-svn-id: svn://svn.code.sf.net/p/mulan/code/trunk@22 90ebec12-01d7-49c3-8feb-d42dc2bd4ece

--- mulan/test/testmlknn.java
+++ mulan/test/testmlknn.java
@@ -2,7 +2,6 @@ import java.io.BufferedWriter;
 import java.io.FileReader;
 import java.io.FileWriter;
 
-
 import mulan.Statistics;
 import mulan.classifier.MultiLabelKNN;
 import mulan.evaluation.Evaluation;
@@ -12,41 +11,51 @@ import weka.classifiers.Classifier;
 import weka.classifiers.functions.SMO;
 import weka.classifiers.trees.J48;
 import weka.core.Instances;
+import mulan.evaluation.Evaluation;
+import mulan.evaluation.Evaluator;
 
 public class testmlknn {
 
 	public testmlknn() {
-	} 
+	}
 
 	public static void main(String[] args) throws Exception {
-		
+
 		String path = "E:/Documents and Settings/lefman/Desktop/my workspace/datasets/";
 		String datastem = "yeast";
 		int numLabels = 14;
+		//int numNeighbours = 2;
 
 		FileReader frTrain = new FileReader(path + datastem + "-train.arff");
 		Instances trainData = new Instances(frTrain);
 		FileReader frTest = new FileReader(path + datastem + "-test.arff");
 		Instances testData = new Instances(frTest);
 		
-		MultiLabelKNN mlknn = new MultiLabelKNN(14,2,1);
+		for(int i=1;i<=12;i++){
+		System.out.println("Calculating mlknn output for "+i+" neighbours");
 		
-		mlknn.buildClassifier(trainData);	
+		MultiLabelKNN mlknn = new MultiLabelKNN(numLabels, i, 1,trainData);
+		mlknn.buildClassifier(trainData);
+		// mlknn.output();
 		
-		mlknn.output();
-		
-		
-		 // show multilabel statistics 
-		//Instances allData = new Instances(trainData);
-        //for (int i=0; i<testData.numInstances(); i++)
-        //    allData.add(testData.instance(i));
+		Evaluator eval;
+		Evaluation results;
+		eval = new Evaluator();
+		//results = eval.crossValidate(mlknn, trainData);
+		results = eval.evaluate(mlknn, testData);
+		System.out.println(results.toString());
+		System.gc();
+		}
 
-        Statistics stats = new Statistics();
-        stats.calculateStats(trainData, numLabels);
-        System.out.println(stats.toString());
-		 
-		 /*  // Binary Relevance Classifier Evaluator eval; Evaluation results;
-		 * System.out.println("BR"); BinaryRelevanceClassifier br = new
+		/*
+		 * // show multilabel statistics //Instances allData = new
+		 * Instances(trainData); //for (int i=0; i<testData.numInstances();
+		 * i++) // allData.add(testData.instance(i));
+		 * 
+		 * Statistics stats = new Statistics(); stats.calculateStats(trainData,
+		 * numLabels); System.out.println(stats.toString());
+		 *  /* // Binary Relevance Classifier Evaluator eval; Evaluation
+		 * results; System.out.println("BR"); BinaryRelevanceClassifier br = new
 		 * BinaryRelevanceClassifier();
 		 * br.setBaseClassifier(Classifier.makeCopy(baseClassifier));
 		 * br.setNumLabels(numLabels); br.buildClassifier(trainData); eval = new
