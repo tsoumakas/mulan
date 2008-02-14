6f47654f0b363c4c5a61f1bc5f58f00167588c58
Grigorios Tsoumakas <greg@csd.auth.gr>
2008-02-14 22:50:18 +0000

    new class
    git-svn-id: svn://svn.code.sf.net/p/mulan/code/trunk@49 90ebec12-01d7-49c3-8feb-d42dc2bd4ece

---
+++ mulan/src/mulan/attributeSelection/LabelPowersetAttributeEvaluator.java
@@ -0,0 +1,41 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package mulan.attributeSelection;
+
+import mulan.Transformations;
+import weka.attributeSelection.AttributeEvaluator;
+import weka.core.Instances;
+
+/**
+ *
+ * @author greg
+ */
+public class LabelPowersetAttributeEvaluator extends AttributeEvaluator {
+    // number of labels
+    int numLabels;
+    // The single-label attributed evaluator to use underneath
+    AttributeEvaluator baseAttributeEvaluator;
+    
+    public LabelPowersetAttributeEvaluator(int l) {
+        numLabels = l;
+    } 
+    
+    @Override
+    public double evaluateAttribute(int attribute) throws Exception {
+        return baseAttributeEvaluator.evaluateAttribute(attribute);
+    }
+
+    @Override
+    public void buildEvaluator(Instances data) throws Exception {
+        Transformations trans = new Transformations(numLabels);
+        Instances newData = trans.LabelPowerset(data);
+        baseAttributeEvaluator.buildEvaluator(newData);
+    }
+
+    public void setAttributeEvaluator(AttributeEvaluator x) {
+        baseAttributeEvaluator = x;
+    }
+}
