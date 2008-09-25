8ca4dd90ac905d3cafad76c230321967c8f0719f
Grigorios Tsoumakas <greg@csd.auth.gr>
2008-09-25 18:24:39 +0000

    new class for ranking attributes based on individual evaluation
    git-svn-id: svn://svn.code.sf.net/p/mulan/code/trunk/mulan@118 90ebec12-01d7-49c3-8feb-d42dc2bd4ece

---
+++ src/mulan/attributeSelection/Ranker.java
@@ -0,0 +1,31 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package mulan.attributeSelection;
+
+import weka.attributeSelection.ASEvaluation;
+import weka.core.Instances;
+
+/**
+ * 
+ * @author greg
+ */
+public class Ranker extends weka.attributeSelection.Ranker {
+    private int numLabels;
+    
+    public Ranker(int l) {
+        numLabels = l;
+    }
+    
+    @Override
+    public int[] search (ASEvaluation ASEval, Instances data) throws Exception 
+    {
+        String startSet = "" + data.numAttributes();
+        for (int i=0; i<numLabels-1; i++)
+            startSet += "," + (data.numAttributes()-i-1);
+        setStartSet(startSet);
+        return super.search(ASEval, data);
+    }
+}
