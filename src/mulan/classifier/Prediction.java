3ddf69513d6fea320371d2414046677589284c10
Grigorios Tsoumakas <greg@csd.auth.gr>
2008-11-25 18:32:13 +0000

    Added a setter for predictions
    git-svn-id: svn://svn.code.sf.net/p/mulan/code/trunk/mulan@143 90ebec12-01d7-49c3-8feb-d42dc2bd4ece

--- src/mulan/classifier/Prediction.java
+++ src/mulan/classifier/Prediction.java
@@ -1,16 +1,39 @@
 package mulan.classifier;
+
+/*
+ *    This program is free software; you can redistribute it and/or modify
+ *    it under the terms of the GNU General Public License as published by
+ *    the Free Software Foundation; either version 2 of the License, or
+ *    (at your option) any later version.
+ *
+ *    This program is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *    GNU General Public License for more details.
+ *
+ *    You should have received a copy of the GNU General Public License
+ *    along with this program; if not, write to the Free Software
+ *    Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+ */
+
 import weka.core.Utils;
-
-
 
 /**
  * Simple container class for multilabel classification result 
  */
+
 public class Prediction {
 
 	protected double[] confidences;
 	protected double[] predictedLabels;
-		
+
+	public Prediction(double[] labels, double[] confidences)
+	{
+		this.confidences = confidences;
+		predictedLabels = labels;
+	}
+        
+        
 	public double[] getConfidences()
 	{
 		return confidences;
@@ -26,21 +49,29 @@ public class Prediction {
 		return Utils.eq(1, predictedLabels[label]);
 	}
 	
+        /**
+         * 
+         * @param label: the index of a label
+         * @param value: whether this label is predicted as true or not
+         */
+        public void setPrediction(int label, boolean value) 
+        {
+                if (value)
+                    predictedLabels[label] = 1;
+                else
+                    predictedLabels[label] = 0;
+        }   
+        
+        
 	/**
-	 * Is confidence a proper name?
-	 * @param label
-	 * @return
+	 * @param label: the index of a label
+	 * @return the confidence of the truth (1) of this label
 	 */
 	public double getConfidence(int label)
 	{
 		return confidences[label];
 	}
-	
-	public Prediction(double[] labels, double[] confidences)
-	{
-		this.confidences = confidences;
-		predictedLabels = labels;
-	}
+                
 	
 	/**
 	 * Number of predicted labels for this instance. 
