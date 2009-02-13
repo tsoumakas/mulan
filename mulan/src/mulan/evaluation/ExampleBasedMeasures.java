f37dbd6b67313aa0fd3eff62f83c62ed13c9d3ba
Grigorios Tsoumakas <greg@csd.auth.gr>
2009-02-13 22:34:04 +0000

    A first attempt on ExampleBasedMeasures
    git-svn-id: svn://svn.code.sf.net/p/mulan/code/branches/mulan-eval-refactor@187 90ebec12-01d7-49c3-8feb-d42dc2bd4ece

--- mulan/src/mulan/evaluation/ExampleBasedMeasures.java
+++ mulan/src/mulan/evaluation/ExampleBasedMeasures.java
@@ -4,9 +4,18 @@ import java.util.List;
 
 import mulan.classifier.Bipartition;
 import sun.reflect.generics.reflectiveObjects.NotImplementedException;
+import weka.core.Utils;
 
 
 public class ExampleBasedMeasures {
+	private double hammingLoss;
+	private double subsetAccuracy;
+	private double accuracy;
+	private double recall;
+	private double precision;
+	private double fmeasure;
+
+	private double forgivenessRate;
 
 	
 	protected ExampleBasedMeasures(List<ModelEvaluationDataPair<Bipartition>> predictionData){
@@ -16,14 +25,94 @@ public class ExampleBasedMeasures {
 	protected ExampleBasedMeasures(ModelCrossValidationDataSet<Bipartition> crossValPredictionDataSet){
 		compute(crossValPredictionDataSet);
 	}
-	
+
+    // shouldn't the following be private?
 	protected void compute(List<ModelEvaluationDataPair<Bipartition>> predictionData){
-		throw new NotImplementedException();
+		// Reset in case of multiple calls
+		accuracy = 0;
+		hammingLoss = 0;
+		precision = 0;
+		recall = 0;
+		fmeasure = 0;
+		subsetAccuracy = 0;
+
+		int numLabels = predictionData.get(0).getTrueLabels().size();
+        for (ModelEvaluationDataPair<Bipartition> pair : predictionData) 
+		{
+			// Counter variables
+			double setUnion = 0; // |Y or Z|
+			double setIntersection = 0; // |Y and Z|
+			double labelPredicted = 0; // |Z|
+			double labelActual = 0; // |Y|
+			double symmetricDifference = 0; // |Y xor Z|
+			boolean setsIdentical = true; // innocent until proven guilty
+
+			//Do the counting
+            List<Boolean> trueLabels = pair.getTrueLabels(); 
+            List<Boolean> predictedLabels = pair.getModelOutput().getBipartition();
+			for (int j = 0; j < numLabels; j++)
+			{
+				boolean actual = trueLabels.get(j);
+				boolean predicted = predictedLabels.get(j);
+
+				if (predicted != actual)
+				{
+					symmetricDifference++;
+					setsIdentical = false;
+				}
+
+				if (actual) labelActual++;
+				if (predicted) labelPredicted++;
+
+				if (predicted && actual) setIntersection++;
+				if (predicted || actual) setUnion++;
+			}
+
+			if (setsIdentical) subsetAccuracy++;
+
+			if(labelActual + labelPredicted == 0)
+			{
+				accuracy  += 1;
+				recall    += 1;
+				precision += 1;
+				fmeasure  += 1;
+			}
+			else
+			{
+				if (Utils.eq(forgivenessRate, 1.0)) accuracy += (setIntersection / setUnion);
+				else accuracy += Math.pow(setIntersection / setUnion, forgivenessRate);
+
+				if (labelPredicted > 0) precision += (setIntersection / labelPredicted);
+				if (labelActual > 0)    recall += (setIntersection / labelActual);
+				
+			}
+			hammingLoss += (symmetricDifference / numLabels);
+		}
+
+		// Set final values
+		int numInstances = predictionData.size();
+		hammingLoss /= numInstances;
+		accuracy /= numInstances;
+		precision /= numInstances;
+		recall /= numInstances;
+		subsetAccuracy /= numInstances;
+		fmeasure = computeF1Measure(precision, recall);
+    }
+
+  	private double computeF1Measure(double precision, double recall)
+	{
+	    if (Utils.eq(precision + recall, 0))
+            return 0;
+	    else
+            return (2 * precision * recall) / (precision + recall);
 	}
-	
+
 	protected void compute(ModelCrossValidationDataSet<Bipartition> crossValPredictionDataSet){
-		throw new NotImplementedException();
-	}
+        int numFolds = crossValPredictionDataSet.getNumFolds();
+        for (int i=0; i<numFolds; i++) {
+            List<ModelEvaluationDataPair<Bipartition>> predictionData = crossValPredictionDataSet.getFoldData(i);
+        }
+    }
 	
 	public double getHammingLoss(){
 		throw new NotImplementedException();
