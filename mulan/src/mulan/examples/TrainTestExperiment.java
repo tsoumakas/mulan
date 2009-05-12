8040423ab5deae34dcf8246c1138b7e350596b18
Grigorios Tsoumakas <greg@csd.auth.gr>
2009-05-12 07:54:19 +0000

    updated example
    git-svn-id: svn://svn.code.sf.net/p/mulan/code/trunk@253 90ebec12-01d7-49c3-8feb-d42dc2bd4ece

--- mulan/src/mulan/examples/TrainTestExperiment.java
+++ mulan/src/mulan/examples/TrainTestExperiment.java
@@ -1,15 +1,12 @@
 package mulan.examples;
 
-import java.io.FileReader;
 import mulan.classifier.transformation.BinaryRelevance;
+import mulan.classifier.transformation.CalibratedLabelRanking;
 import mulan.classifier.transformation.LabelPowerset;
-import mulan.classifier.transformation.MultiClassLearner;
 import mulan.core.data.MultiLabelInstances;
 import mulan.evaluation.Evaluation;
 import mulan.evaluation.Evaluator;
-import mulan.transformations.multiclass.*;
 import weka.classifiers.trees.J48;
-import weka.core.Instances;
 import weka.core.Utils;
 
 /**
@@ -19,48 +16,48 @@ import weka.core.Utils;
 public class TrainTestExperiment {
 
     public static void main(String[] args) {
-        long before, after;
-        double paramTime, buildTime, testTime;
-        int numExperiments = 10;
+        String[] methodsToCompare = {"LP", "CLR", "BR"};
 
         try {
             String path = Utils.getOption("path", args);
             String filestem = Utils.getOption("filestem", args);
-            int labels = Integer.parseInt(Utils.getOption("labels",args));
-
-            MultiLabelInstances train = new MultiLabelInstances(path + filestem + "-train.arff", labels);
-            MultiLabelInstances test = new MultiLabelInstances(path + filestem + "-test.arff", labels);
+            MultiLabelInstances train = new MultiLabelInstances(path + filestem + "-train.arff", path + filestem + ".xml");
+            MultiLabelInstances test = new MultiLabelInstances(path + filestem + "-test.arff", path + filestem + ".xml");
             
             Evaluator eval = new Evaluator();
             Evaluation results;
 
-            //* LP
-            System.out.println("LP");
-            J48 lpBaseClassifier = new J48();
-            LabelPowerset lp = new LabelPowerset(lpBaseClassifier);
-            lp.build(train);
-            results = eval.evaluate(lp, test.getDataSet());
-            System.out.println(results.toString());
-            //*/
+            for (int i=0; i<methodsToCompare.length; i++) {
 
-            //* Multiclass Transformations
-            System.out.println("Multiclass Transformations - Copy");
-            J48 mcBaseClassifier = new J48();
-            MultiClassTransformation copy = new Copy(labels);
-            MultiClassLearner mc;
-            mc = new MultiClassLearner(mcBaseClassifier, copy);
-            mc.build(train);
-            results = eval.evaluate(mc, test.getDataSet());
-            System.out.println(results.toString());
+                if (methodsToCompare[i].equals("BR")) {
+                    System.out.println(methodsToCompare[i]);
+                    J48 brClassifier = new J48();
+                    BinaryRelevance br = new BinaryRelevance(brClassifier);
+                    br.build(train);
+                    results = eval.evaluate(br, test);
+                    System.out.println(results.toString());
+                }
 
-            //* BR
-            System.out.println("BR");
-            J48 brClassifier = new J48();
-            BinaryRelevance br = new BinaryRelevance(brClassifier);
-            br.build(train);
-            results = eval.evaluate(br, test.getDataSet());
-            System.out.println(results.toString());
-            //*/
+                if (methodsToCompare[i].equals("CLR")) {
+                    System.out.println(methodsToCompare[i]);
+                    J48 clrClassifier = new J48();
+                    CalibratedLabelRanking clr = new CalibratedLabelRanking(clrClassifier);
+                    clr.setDebug(true);
+                    clr.build(train);
+                    results = eval.evaluate(clr, test);
+                    System.out.println(results.toString());
+                }
+
+                if (methodsToCompare[i].equals("LP")) {
+                    System.out.println(methodsToCompare[i]);
+                    J48 lpBaseClassifier = new J48();
+                    LabelPowerset lp = new LabelPowerset(lpBaseClassifier);
+                    lp.setDebug(true);
+                    lp.build(train);
+                    results = eval.evaluate(lp, test);
+                    System.out.println(results.toString());
+                }
+            }
         }
         catch (Exception e) {
             e.printStackTrace();
