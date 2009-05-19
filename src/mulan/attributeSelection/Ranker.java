77c43da7f7ac66756191a3b8bb3dddfda2286236
Grigorios Tsoumakas <greg@csd.auth.gr>
2009-05-19 22:28:06 +0000

    Final fixes
    git-svn-id: svn://svn.code.sf.net/p/mulan/code/trunk/mulan@273 90ebec12-01d7-49c3-8feb-d42dc2bd4ece

--- src/mulan/attributeSelection/Ranker.java
+++ src/mulan/attributeSelection/Ranker.java
@@ -1,3 +1,24 @@
+/*
+ *    This program is free software; you can redistribute it and/or modify
+ *    it under the terms of the GNU General Public License as published by
+ *    the Free Software Foundation; either version 2 of the License, or
+ *    (at your option) any later version.
+ *
+ *    This program is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *    GNU General Public License for more details.
+ *
+ *    You should have received a copy of the GNU General Public License
+ *    along with this program; if not, write to the Free Software
+ *    Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+ */
+
+/*
+ *    Ranker.java
+ *    Copyright (C) 2009 Aristotle University of Thessaloniki, Thessaloniki, Greece
+ */
+
 package mulan.attributeSelection;
 
 import mulan.core.data.MultiLabelInstances;
@@ -7,14 +28,15 @@ import weka.core.Instances;
 import weka.core.Range;
 
 /**
- * 
- * @author greg
+ * Ranks attributes according to an AttributeEvaluator. It internally uses Weka's
+ * Ranker, initialized so as to neglect the labels.
+ *
+ * @author Grigorios Tsoumakas
  */
 public class Ranker
-{
-	
+{	
 	/**
-	 * Calls a specifier {@link AttributeEvaluator} to evaluate each feature attribute
+	 * Calls a specified {@link AttributeEvaluator} to evaluate each feature attribute
 	 * of specified {@link MultiLabelInstances} data set. 
 	 * Internally it uses {@link weka.attributeSelection.Ranker}, where 
 	 * {@link weka.attributeSelection.Ranker#setStartSet(String)} is preset with range of
@@ -30,8 +52,7 @@ public class Ranker
         Instances data = mlData.getDataSet();
         String startSet = Range.indicesToRangeList(mlData.getLabelIndices());
 	    weka.attributeSelection.Ranker wekaRanker = new weka.attributeSelection.Ranker();
-	    wekaRanker.setStartSet(startSet);
-	    
-        return wekaRanker.search((ASEvaluation)attributeEval, data);
+	    wekaRanker.setStartSet(startSet);	    
+        return  wekaRanker.search((ASEvaluation) attributeEval, data);
     }
 }
