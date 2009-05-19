bb335892c8a23def5988a0cfb89a8b251d35b68a
Grigorios Tsoumakas <greg@csd.auth.gr>
2009-05-19 22:31:04 +0000

    Added Header information and some JavaDoc
    git-svn-id: svn://svn.code.sf.net/p/mulan/code/trunk/mulan@274 90ebec12-01d7-49c3-8feb-d42dc2bd4ece

--- src/mulan/examples/AttributeSelectionTest.java
+++ src/mulan/examples/AttributeSelectionTest.java
@@ -1,10 +1,24 @@
 /*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
+ *    This program is free software; you can redistribute it and/or modify
+ *    it under the terms of the GNU General Public License as published by
+ *    the Free Software Foundation; either version 2 of the License, or
+ *    (at your option) any later version.
+ *
+ *    This program is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *    GNU General Public License for more details.
+ *
+ *    You should have received a copy of the GNU General Public License
+ *    along with this program; if not, write to the Free Software
+ *    Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
  */
 
+/*
+ *    AttributeSelectionTest.java
+ *    Copyright (C) 2009 Aristotle University of Thessaloniki, Thessaloniki, Greece
+ */
 package mulan.examples;
-
 
 import java.util.Arrays;
 import mulan.attributeSelection.LabelPowersetAttributeEvaluator;
@@ -18,6 +32,7 @@ import weka.filters.Filter;
 import weka.filters.unsupervised.attribute.Remove;
 
 /**
+ * Demonstrates the attribute selection capabilities of Mulan
  *
  * @author greg
  */
