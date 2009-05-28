2491388a49b202b408082e3efaeb4b7d28cd4549
Grigorios Tsoumakas <greg@csd.auth.gr>
2009-05-28 23:10:56 +0000

    HOMER
    git-svn-id: svn://svn.code.sf.net/p/mulan/code/trunk/mulan@280 90ebec12-01d7-49c3-8feb-d42dc2bd4ece

---
+++ src/mulan/classifier/transformation/HOMER.java
@@ -0,0 +1,110 @@
+/*
+ *    This program is free software; you can redistribute it and/or modify
+ *    it under the terms of the GNU General Public License as published by
+ *    the Free Software Foundation; either version 2 of the License, or
+ *    (at your option) any later version.
+ *
+ *    This program is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *    GNU General Public License for more details.
+ *
+ *    You should have received a copy of the GNU General Public License
+ *    along with this program; if not, write to the Free Software
+ *    Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+ */
+
+/*
+ *    HOMER.java
+ *    Copyright (C) 2009 Aristotle University of Thessaloniki, Thessaloniki, Greece
+ */
+package mulan.classifier.transformation;
+
+import mulan.classifier.MultiLabelLearner;
+import mulan.classifier.MultiLabelOutput;
+import mulan.classifier.hierarchical.HMC;
+import mulan.core.data.MultiLabelInstances;
+import mulan.classifier.hierarchical.HierarchyBuilder;
+import weka.core.Instance;
+import weka.core.Instances;
+import weka.core.TechnicalInformation;
+import weka.core.TechnicalInformation.*;
+
+/**
+ * <!-- globalinfo-start -->
+ *
+ * <pre>
+ * Class implementing the HOMER algorithm
+ * </pre>
+ *
+ * For more information:
+ *
+ * <pre>
+ * G. Tsoumakas, I. Katakis, I. Vlahavas, "Effective and Efficient Multilabel
+ * Classification in Domains with Large Number of Labels", Proc. ECML/PKDD 2008
+ * Workshop on Mining Multidimensional Data (MMD'08), Antwerp, Belgium, 2008.
+ * </pre>
+ *
+ * <!-- globalinfo-end -->
+ *
+ * <!-- technical-bibtex-start --> BibTeX:
+ * 
+ * <!-- technical-bibtex-end -->
+ *
+ * @author Grigorios Tsoumakas
+ * @revisions 0.01
+ */
+public class HOMER extends MultiLabelMetaLearner
+{
+    private final int numClusters;
+    private HMC hmc;
+    private HierarchyBuilder hb;
+    private Instances header;
+
+    public HOMER(MultiLabelLearner mll, int clusters)
+    {
+        super(mll);
+        numClusters = clusters;
+    }
+
+    @Override
+    protected void buildInternal(MultiLabelInstances trainingSet) throws Exception {
+        debug("Constructing the hierarchical multilabel dataset");
+        hb = new HierarchyBuilder(numClusters, HierarchyBuilder.Method.Random);
+        MultiLabelInstances meta = hb.buildHierarchy(trainingSet);
+        header = new Instances(meta.getDataSet(), 0);
+        debug("Training the hierarchical classifier");
+        hmc = new HMC(baseLearner);
+        hmc.setDebug(getDebug());
+        hmc.build(meta);
+        //System.out.println(meta.getDataSet());
+    }
+
+    public MultiLabelOutput makePrediction(Instance instance) throws Exception {
+        Instance transformed = hb.modifyInstance(instance);
+        transformed.setDataset(header);
+        MultiLabelOutput mlo = hmc.makePrediction(transformed);
+        boolean[] oldBipartition = mlo.getBipartition();
+        //System.out.println("old:" + Arrays.toString(oldBipartition));
+        boolean[] newBipartition = new boolean[numLabels];
+        System.arraycopy(oldBipartition, 0, newBipartition, 0, numLabels);
+        //System.out.println("new:" + Arrays.toString(newBipartition));
+        double[] oldConfidences = mlo.getConfidences();
+        double[] newConfidences = new double[numLabels];
+        System.arraycopy(oldConfidences, 0, newConfidences, 0, numLabels);
+        MultiLabelOutput newMLO = new MultiLabelOutput(newBipartition, newConfidences);
+        return newMLO;
+    }
+
+    @Override
+    public TechnicalInformation getTechnicalInformation() {
+		TechnicalInformation result = new TechnicalInformation(Type.INPROCEEDINGS);
+		result.setValue(Field.AUTHOR, "Grigorios Tsoumakas and Ioannis Katakis and Ioannis Vlahavas");
+		result.setValue(Field.TITLE, "Effective and Efficient Multilabel Classification in Domains with Large Number of Labels");
+		result.setValue(Field.BOOKTITLE, "Proc. ECML/PKDD 2008 Workshop on Mining Multidimensional Data (MMD'08)");
+		result.setValue(Field.LOCATION, "Antwerp, Belgium");
+		result.setValue(Field.YEAR, "2008");
+		return result;
+    }
+
+}
