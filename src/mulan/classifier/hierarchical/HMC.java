a727105339bb98fc13f54f35985a9c88c37b2c7b
Grigorios Tsoumakas <greg@csd.auth.gr>
2009-05-28 23:03:30 +0000

    Hierarchical Multilabel Classifier
    git-svn-id: svn://svn.code.sf.net/p/mulan/code/trunk/mulan@279 90ebec12-01d7-49c3-8feb-d42dc2bd4ece

---
+++ src/mulan/classifier/hierarchical/HMC.java
@@ -0,0 +1,338 @@
+/*
+ *    This program is free software; you can redistribute it and/or modify
+ *    it under the terms of the GNU General Public License as published by
+ *    the Free Software Foundation; either version 2 of the License, or
+ *    (at your option) any later version.
+ *
+ *    This program is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *    GNU General Public License for more details.
+ *
+ *    You should have received a copy of the GNU General Public License
+ *    along with this program; if not, write to the Free Software
+ *    Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+ */
+
+/*
+ *    HMC.java
+ *    Copyright (C) 2009 Aristotle University of Thessaloniki, Thessaloniki, Greece
+ */
+package mulan.classifier.hierarchical;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Set;
+import mulan.classifier.MultiLabelLearner;
+import mulan.classifier.MultiLabelOutput;
+import mulan.classifier.transformation.MultiLabelMetaLearner;
+import mulan.core.data.InvalidDataFormatException;
+import mulan.core.data.LabelNode;
+import mulan.core.data.LabelNodeImpl;
+import mulan.core.data.LabelsMetaData;
+import mulan.core.data.LabelsMetaDataImpl;
+import mulan.core.data.MultiLabelInstances;
+import mulan.transformations.RemoveAllLabels;
+import weka.core.Instance;
+import weka.core.Instances;
+import weka.core.TechnicalInformation;
+import weka.filters.Filter;
+import weka.filters.unsupervised.attribute.Remove;
+
+/**
+ * Class that implements a Hierarchical Multilabel classifier (HMC).
+ * HMC classifier takes as parameter any kind of multilabel classifier and
+ * builds a hierarchy. Any node of hierarchy is a classifier and is trained
+ * separately. The root classifier is trained on all data and as getting down
+ * the hierarchy tree the data is adjusted properly to each node. Firstly,
+ * instances that do not belong to the node are removed and then attributes that
+ * are unnecessary are removed also.
+ *
+ * @author George Saridis
+ * @version 0.2
+ */
+public class HMC extends MultiLabelMetaLearner {
+
+    private LabelsMetaData originalMetaData;
+    private HMCNode root;
+    private HashMap<String, Integer> labelsAndIndices;
+
+    public HMC(MultiLabelLearner baseLearner) throws Exception {
+        super(baseLearner);
+    }
+
+    public HMC(MultiLabelLearner baseLearner, MultiLabelInstances dataSet) throws Exception {
+        this(baseLearner);
+        build(dataSet);
+    }
+
+    @Override
+    public TechnicalInformation getTechnicalInformation() {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    private void buildRec(HMCNode node, Instances data) throws InvalidDataFormatException, Exception
+    {
+        String metaLabel = node.getName();
+        debug(metaLabel);
+
+        debug("Preparing node data");
+        Set<String> childrenLabels = new HashSet<String>();
+        Set<String> currentlyAvailableLabels = new HashSet<String>();
+        if (metaLabel.equals("root")) {
+            for (LabelNode child : originalMetaData.getRootLabels())
+                childrenLabels.add(child.getName());
+            currentlyAvailableLabels = originalMetaData.getLabelNames();
+        } else {
+            LabelNode labelNode = originalMetaData.getLabelNode(metaLabel);
+            for (LabelNode child : labelNode.getChildren())
+                childrenLabels.add(child.getName());
+            currentlyAvailableLabels = labelNode.getDescendantLabels();
+        }
+
+        // delete non-children labels
+        Set<String> labelsToDelete = new HashSet(currentlyAvailableLabels);
+        labelsToDelete.removeAll(childrenLabels);
+        System.out.println("Children: " + Arrays.toString(childrenLabels.toArray()));
+        System.out.println("Labels to delete:" + Arrays.toString(labelsToDelete.toArray()));
+
+        int[] indicesToDelete = new int[labelsToDelete.size()];
+        int counter1 = 0;
+        for (String label : labelsToDelete) {
+            indicesToDelete[counter1] = data.attribute(label).index();
+            counter1++;
+        }
+
+        Remove filter1 = new Remove();
+        filter1.setAttributeIndicesArray(indicesToDelete);
+        filter1.setInputFormat(data);
+        Instances nodeInstances = Filter.useFilter(data, filter1);
+//        System.out.println()
+
+        // create meta data
+        LabelsMetaDataImpl nodeMetaData = new LabelsMetaDataImpl();
+        for (String label : childrenLabels)
+            nodeMetaData.addRootNode(new LabelNodeImpl(label));
+
+        // create multi-label instance
+        MultiLabelInstances nodeData = new MultiLabelInstances(nodeInstances, nodeMetaData);
+        
+        debug("Building model");
+        node.build(nodeData);
+
+        for (String childLabel : childrenLabels) {
+            LabelNode childNode = originalMetaData.getLabelNode(childLabel);
+            if (!childNode.hasChildren())
+                continue;
+            debug("Preparing child data");
+
+            // remove instances where child is 0
+            int childMetaLabelIndex = data.attribute(childLabel).index();
+            Instances childData = new Instances(data);
+            for (int i=0; i<childData.numInstances(); i++)
+                if (childData.instance(i).stringValue(childMetaLabelIndex).equals("0")) {
+                    childData.delete(i);
+                    // While deleting an instance from the trainSet, i must reduced too
+                    i--;
+                }
+
+            // delete non-descendant labels
+            Set<String> descendantLabels = childNode.getDescendantLabels();
+            Set<String> labelsToDelete2 = new HashSet(currentlyAvailableLabels);
+            labelsToDelete2.removeAll(descendantLabels);
+            System.out.println("Labels to delete:" + Arrays.toString(labelsToDelete2.toArray()));
+            int[] indicesToDelete2 = new int[labelsToDelete2.size()];
+            int counter2 = 0;
+            for (String label : labelsToDelete2) {
+                indicesToDelete2[counter2] = childData.attribute(label).index();
+                counter2++;
+            }
+
+            Remove filter2 = new Remove();
+            filter2.setAttributeIndicesArray(indicesToDelete2);
+            filter2.setInputFormat(childData);
+            childData = Filter.useFilter(childData, filter2);
+
+            MultiLabelLearner mll = baseLearner.makeCopy();
+            HMCNode child = new HMCNode(childLabel, mll);
+            node.addChild(child);
+
+            buildRec(child, childData);
+        }
+
+    }
+
+    @Override
+    protected void buildInternal(MultiLabelInstances dataSet) throws Exception {
+        originalMetaData = dataSet.getLabelsMetaData();
+        Set<String> rootLabels = new HashSet<String>();
+        for (LabelNode node : originalMetaData.getRootLabels())
+            rootLabels.add(node.getName());
+
+
+
+        MultiLabelLearner mll = baseLearner.makeCopy();
+        root = new HMCNode("root", mll);
+        buildRec(root, dataSet.getDataSet());
+
+        labelsAndIndices = dataSet.getLabelsOrder();
+    }
+
+    public MultiLabelOutput makePrediction(Instance instance) throws Exception {
+        boolean[] predictedLabels = new boolean[numLabels];
+        double[] confidences = new double[numLabels];
+
+        makePrediction(root, instance, predictedLabels, confidences);
+
+        return new MultiLabelOutput(predictedLabels, confidences);
+    }
+
+    private void makePrediction(HMCNode currentNode, Instance instance, boolean[] predictedLabels, double[] confidences) throws Exception {
+        String metaLabel = currentNode.getName();
+        //System.out.println("Node: " + metaLabel);
+
+        double[] values = instance.toDoubleArray();
+
+        Instance transformed = new Instance(1, values);
+        // delete all attributes (other way of transformation)
+        transformed = RemoveAllLabels.transformInstance(transformed, labelIndices);
+        transformed.setDataset(null);
+
+        int numChildren;
+        if (metaLabel.equals("root"))
+            numChildren = originalMetaData.getRootLabels().size();
+        else
+            numChildren = originalMetaData.getLabelNode(metaLabel).getChildren().size();
+        for (int j=0; j<numChildren; j++)
+            transformed.insertAttributeAt(transformed.numAttributes());
+        transformed.setDataset(currentNode.getHeader());
+        // add as many attributes as the children    
+//        System.out.println("header:" + currentNode.getHeader());
+        //System.out.println(transformed.toString());
+
+        //debug("working at node " + currentNode.getName());
+        //debug(Arrays.toString(predictedLabels));
+        MultiLabelOutput pred = currentNode.makePrediction(transformed);
+        int[] indices = currentNode.getLabelIndices();
+        boolean[] temp = pred.getBipartition();
+
+        for (int i=0; i<temp.length; i++)
+        {
+            String childName = currentNode.getHeader().attribute(indices[i]).name();
+            //System.out.println("childName:" + childName);
+            int idx = labelsAndIndices.get(childName);
+            if (pred.getBipartition()[i] == true) {
+                predictedLabels[idx] = true;
+                confidences[idx] = pred.getConfidences()[i];
+                if (currentNode.hasChildren())
+                    for (HMCNode child : currentNode.getChildren())
+                        if (child.getName().equals(childName))
+                            makePrediction(child, instance, predictedLabels, confidences);
+            } else {
+                predictedLabels[idx] = false;
+                Set<String> descendantLabels = originalMetaData.getLabelNode(childName).getDescendantLabels();
+                if (descendantLabels != null)
+                    for (String label : descendantLabels) {
+                        int idx2 = labelsAndIndices.get(label);
+                        predictedLabels[idx2] = false;
+                        confidences[idx2] = pred.getConfidences()[i];
+                    }
+            }
+        }
+    }
+
+    /**
+     * Learns the Labels hierarchy and builds the appropriate tree for the
+     * hierarchical multilabel classifier. The method is recursive.
+     *
+     * @param labels the set of labels that will be added to the HMC_node
+     * @param currentNode the node to which the labels are added
+     */
+    private void learnHierarchy(Set<LabelNode> labels, HMCNode currentNode) throws Exception {
+        for (LabelNode labelNode : labels) {
+            HMCNode newHMCNode = new HMCNode(labelNode.getName(), baseLearner);
+            if (labelNode.hasChildren()) {
+                learnHierarchy(labelNode.getChildren(), newHMCNode);
+            }
+            currentNode.addChild(newHMCNode);
+        }
+    }
+
+
+
+    /**
+     * Deletes the unnecessary attributes. Actually keeps only the children
+     * names of the node that is going to be trained as attributes and deletes
+     * the rest.
+     *
+     * @param data the instances from which the attributes will be removed
+     * @param labelName the name of the node whose children will be kept as attributes
+     * @return MultiLabelInstances
+     * @throws mulan.core.data.InvalidDataFormatException
+     */
+    protected MultiLabelInstances deleteLabels(MultiLabelInstances mlData, String currentLabel, boolean keepSubTree) throws InvalidDataFormatException {
+        LabelsMetaData currentMetaData = mlData.getLabelsMetaData();
+        LabelNodeImpl currentLabelNode = (LabelNodeImpl) currentMetaData.getLabelNode(currentLabel);
+
+        Set<String> labelsToKeep;
+        Set<String> allLabels = mlData.getLabelsMetaData().getLabelNames();
+        LabelsMetaDataImpl labelsMetaData = new LabelsMetaDataImpl();
+
+        //Prepare the appropriate labelsMetaData
+        if(keepSubTree){
+            labelsToKeep = currentLabelNode.getDescendantLabels();
+            for (String rootLabel : currentLabelNode.getChildrenLabels()) {
+                LabelNodeImpl rootNode = new LabelNodeImpl(rootLabel);
+                if(mlData.getLabelsMetaData().getLabelNode(rootLabel).hasChildren()){
+                    append(rootNode,mlData.getLabelsMetaData());
+                }
+                labelsMetaData.addRootNode(rootNode);
+            }
+        }else{
+            labelsToKeep = currentLabelNode.getChildrenLabels();
+            for(String rootLabel : labelsToKeep){
+                LabelNodeImpl rootNode = new LabelNodeImpl(rootLabel);
+                labelsMetaData.addRootNode(rootNode);
+            }
+        }
+        
+        debug("Labels: " + labelsMetaData.getLabelNames().toString());
+        
+        //Deleting labels from instances
+        for (String label : allLabels) {
+            if (!labelsToKeep.contains(label)) {
+                int idx = mlData.getDataSet().attribute(label).index();
+                mlData.getDataSet().deleteAttributeAt(idx);
+            }
+        }
+
+        return new MultiLabelInstances(mlData.getDataSet(),labelsMetaData);
+    }
+    private void append(LabelNodeImpl labelNode,LabelsMetaData labelsMetaData){
+        LabelNode father = labelsMetaData.getLabelNode(labelNode.getName());
+        for(LabelNode child : father.getChildren()){
+            LabelNodeImpl newLabelNode = new LabelNodeImpl(child.getName());
+            if (child.hasChildren())
+                append(newLabelNode,labelsMetaData);
+            labelNode.addChildNode(newLabelNode);
+        }
+    }
+    /**
+     * Deletes the unnecessary instances, the instances that have value 0 on
+     * given attribute.
+     *
+     * @param trainSet the trainSet on which the deletion will be applied
+     * @param attrIndex the index of the attribute that the deletion is based
+     */
+    protected void deleteInstances(Instances trainSet, int attrIndex) {
+        for (int i = 0; i < trainSet.numInstances(); i++) {
+            if (trainSet.instance(i).stringValue(attrIndex).equals("0")) {
+                trainSet.delete(i);
+                // While deleting an instance from the trainSet, i must reduced too
+                i--;
+            }
+        }
+    }
+}
---
+++ src/mulan/classifier/hierarchical/HierarchyBuilder.java
@@ -0,0 +1,309 @@
+/*
+ *    This program is free software; you can redistribute it and/or modify
+ *    it under the terms of the GNU General Public License as published by
+ *    the Free Software Foundation; either version 2 of the License, or
+ *    (at your option) any later version.
+ *
+ *    This program is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *    GNU General Public License for more details.
+ *
+ *    You should have received a copy of the GNU General Public License
+ *    along with this program; if not, write to the Free Software
+ *    Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+ */
+
+/*
+ *    HierarchyBuilder.java
+ *    Copyright (C) 2009 Aristotle University of Thessaloniki, Thessaloniki, Greece
+ */
+package mulan.classifier.hierarchical;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Random;
+import java.util.Set;
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.transform.*;
+import javax.xml.transform.dom.DOMSource;
+import javax.xml.transform.stream.StreamResult;
+import org.w3c.dom.Document;
+import mulan.core.data.LabelNode;
+import mulan.core.data.LabelNodeImpl;
+import mulan.core.data.LabelsMetaData;
+import mulan.core.data.LabelsMetaDataImpl;
+import mulan.core.data.MultiLabelInstances;
+import org.w3c.dom.Element;
+import weka.core.Attribute;
+import weka.core.FastVector;
+import weka.core.Instance;
+import weka.core.Instances;
+import weka.core.converters.ArffSaver;
+
+/**
+ * Class that builds a hierarchy on flat lables of given mulltilabel data.
+ * The hierarchy may be built with three methods.
+ *
+ * @author George Saridis
+ * @author Grigorios Tsoumakas
+ * @version 0.1
+ */
+public class HierarchyBuilder {
+
+    private int numPartitions;
+    private Document labelsXMLDoc;
+    private int numMetaLabels;
+    private boolean built = false;
+
+
+    public HierarchyBuilder(int partitions, Method method) {
+        numPartitions = partitions;
+    }
+    /**
+     * Builds a random hierarchy on the labels of multilabel data.
+     *
+     * Firstly a xml file is created with the hierarchy of labels. The flat labels
+     * that were in the initial xml file now will be the leafs of the new hierarchy.
+     * The new labels that are added to the xml are called 'metaLabel' and the xml
+     * file is called 'metaLabels.xml'. Besides that a new arff file is created too.
+     * It contains the same instances as the initial one, which were modified properly
+     * to the new hierarchy.
+     *
+     * @param mlData the multiLabel data on with the new hierarchy will be built
+     * @return the new multiLabel data
+     * @throws java.lang.Exception
+     */
+    public MultiLabelInstances buildHierarchy(MultiLabelInstances mlData) throws Exception {
+        if (mlData.getNumLabels() <= numPartitions) {
+            throw new Exception("Labels are less than the number of clusters you asked! (or equal)");
+        }
+
+        System.out.println("Constructing the label hierarchy");
+        LabelsMetaDataImpl labelsMetaData = createLabelsMetaData(mlData);
+        Set<String> labels = labelsMetaData.getLabelNames();
+        System.out.println(Arrays.toString(labels.toArray()));
+
+        System.out.println("Creating the hierarchical data set");
+        Instances newDataSet = createHierarchicalDataset(mlData, labelsMetaData);
+
+        built = true;
+        return new MultiLabelInstances(newDataSet, labelsMetaData);
+    }
+
+    public MultiLabelInstances buildHierarchyAndSaveFiles(MultiLabelInstances mlData, String arffName, String xmlName) throws Exception {
+        MultiLabelInstances newData = buildHierarchy(mlData);
+        saveToArffFile(newData.getDataSet(), new File(arffName));
+        createXMLFile(mlData.getLabelsMetaData());
+        saveToXMLFile(xmlName);
+        return newData;
+    }
+
+    /**
+     * Adds as many attributes as the number of metaLabels that the hierachyBuilder
+     * added to the dataSet. The new attributes are not initialized.
+     *
+     * @param ins the instance to modify
+     * @return the modified instance
+     */
+    public Instance modifyInstance(Instance ins) {
+        if (built) {
+            Instance modifiedIns = new Instance(ins.weight(), ins.toDoubleArray());
+            for (int i = 0; i < numMetaLabels; i++) {
+                modifiedIns.insertAttributeAt(modifiedIns.numAttributes());
+            }
+            return modifiedIns;
+        }
+        return null;
+    }
+
+    private LabelsMetaDataImpl createLabelsMetaData(MultiLabelInstances mlData) {
+        if (numPartitions > mlData.getNumLabels()) {
+            throw new IllegalArgumentException("Number of labels is smaller than the number of partitions");
+        }
+
+        Set<String> setOfLabels = mlData.getLabelsMetaData().getLabelNames();
+        List<String> listOfLabels = new ArrayList<String>();
+        for (String label : setOfLabels)
+            listOfLabels.add(label);
+
+        ArrayList<String>[] childrenLabels = randomPartitioning(numPartitions, listOfLabels);
+
+        LabelsMetaDataImpl metaData = new LabelsMetaDataImpl();
+        for (int i=0; i<numPartitions; i++) {
+            LabelNodeImpl metaLabel = new LabelNodeImpl("MetaLabel " + (i+1));
+            createLabelsMetaDataRecursive(metaLabel, childrenLabels[i]);
+            metaData.addRootNode(metaLabel);
+        }
+
+        return metaData;
+    }
+
+    private void createLabelsMetaDataRecursive(LabelNodeImpl node, List<String> labels)
+    {
+        int numChildren = Math.min(numPartitions, labels.size());
+
+        ArrayList<String>[] childrenLabels = randomPartitioning(numChildren, labels);
+
+        for (int i=0; i<numChildren; i++) {
+            if (childrenLabels[i].size() > 1) {
+                LabelNodeImpl child = new LabelNodeImpl(node.getName() + "." + (i+1));
+                node.addChildNode(child);
+                createLabelsMetaDataRecursive(child, childrenLabels[i]);
+            } else {
+                LabelNodeImpl child = new LabelNodeImpl(childrenLabels[i].get(0));
+                node.addChildNode(child);
+            }
+        }
+    }
+
+    private ArrayList<String>[] randomPartitioning(int partitions, List<String> labels)
+    {
+        ArrayList<String>[] childrenLabels = new ArrayList[partitions];
+        for (int i=0; i<partitions; i++)
+            childrenLabels[i] = new ArrayList<String>();
+
+        Random rnd = new Random();
+        while (!labels.isEmpty()) {
+            for (int i=0; i<partitions; i++)
+            {
+                if (labels.isEmpty())
+                    break;
+                String rndLabel = labels.remove(rnd.nextInt(labels.size()));
+                childrenLabels[i].add(rndLabel);
+            }
+        }
+        return childrenLabels;
+    }
+
+
+    /**
+     * Creates the hierarchical dataset according to the original multilabel
+     * instances object and the constructed label hierarchy
+     *
+     * @param mlData the original multilabel instances
+     * @param metaData the metadata of the constructed label hierarchy
+     */
+    private Instances createHierarchicalDataset(MultiLabelInstances mlData, LabelsMetaDataImpl metaData) {
+        Set<String> leafLabels = mlData.getLabelsMetaData().getLabelNames();
+        Set<String> metaLabels = metaData.getLabelNames();
+        for (String string : leafLabels) {
+            metaLabels.remove(string);
+        }
+        Instances dataSet = mlData.getDataSet();
+        numMetaLabels = metaLabels.size();
+
+        // copy existing attributes
+        FastVector atts = new FastVector(dataSet.numAttributes()+numMetaLabels);
+        for (int i=0; i<dataSet.numAttributes(); i++)
+            atts.addElement(dataSet.attribute(i));
+
+        FastVector labelValues = new FastVector();
+        labelValues.addElement("0");
+        labelValues.addElement("1");
+
+        // add metalabel attributes
+        for (String metaLabel : metaLabels)
+            atts.addElement(new Attribute(metaLabel, labelValues));
+
+        // initialize dataset
+        Instances newDataSet = new Instances("hierarchical", atts, dataSet.numInstances());
+
+        // copy features and labels, set metalabels
+        for (int i = 0; i < dataSet.numInstances(); i++) {
+            System.out.println("Constructing instance " + (i+1) + "/"  + dataSet.numInstances());
+            // initialize new values
+            double[] newValues = new double[newDataSet.numAttributes()];
+            Arrays.fill(newValues, 0);
+
+            // copy features and labels
+            double[] values = dataSet.instance(i).toDoubleArray();
+            System.arraycopy(values, 0, newValues, 0, values.length);
+            
+            // set metalabels
+            for (String label : leafLabels)
+            {
+                Attribute att = dataSet.attribute(label);
+                if (att.value((int) dataSet.instance(i).value(att)).equals("1"))
+                {
+                    //System.out.println(label);
+                    //System.out.println(Arrays.toString(metaData.getLabelNames().toArray()));
+                    LabelNode currentNode = metaData.getLabelNode(label);
+                    // put 1 all the way up to the root, unless you see a 1, in which case stop
+                    while (currentNode.hasParent()) {
+                        currentNode = currentNode.getParent();
+                        Attribute currentAtt = newDataSet.attribute(currentNode.getName());
+                        // change the following to refer to the array
+                        if (newValues[atts.indexOf(currentAtt)] == 1)
+                            // no need to go more up
+                            break;
+                        else 
+                            // put 1
+                            newValues[atts.indexOf(currentAtt)] = 1;
+                    }
+                }
+            }
+            
+            newDataSet.add(new Instance(dataSet.instance(i).weight(), newValues));
+        }
+        return newDataSet;
+    }
+
+    private void saveToArffFile(Instances dataSet, File file) throws IOException {
+        ArffSaver saver = new ArffSaver();
+        saver.setInstances(dataSet);
+        saver.setFile(file);
+        saver.writeBatch();
+    }
+    
+    private void createXMLFile(LabelsMetaData metaData) throws Exception {
+        DocumentBuilderFactory docBF = DocumentBuilderFactory.newInstance();
+        DocumentBuilder docBuilder = docBF.newDocumentBuilder();
+        labelsXMLDoc = docBuilder.newDocument();
+
+        Element rootElement = labelsXMLDoc.createElement("labels");
+        rootElement.setAttribute("xmlns", "http://mulan.sourceforge.net/labels");
+        labelsXMLDoc.appendChild(rootElement);
+        for (LabelNode rootLabel : metaData.getRootLabels()) {
+            Element newLabelElem = labelsXMLDoc.createElement("label");
+            newLabelElem.setAttribute("name", rootLabel.getName());
+            appendElement(newLabelElem, rootLabel);
+            rootElement.appendChild(newLabelElem);
+        }
+    }
+
+    private void saveToXMLFile(String fileName) {
+        Source source = new DOMSource(labelsXMLDoc);
+        File xmlFile = new File(fileName);
+        StreamResult result = new StreamResult(xmlFile);
+        try {
+            Transformer transformer = TransformerFactory.newInstance().newTransformer();
+            transformer.setOutputProperty(OutputKeys.INDENT, "yes");
+            transformer.setOutputProperty("{http://xml.apache.org/xslt}indent-amount", "4");
+            transformer.setOutputProperty(OutputKeys.METHOD, "xml");
+            transformer.transform(source, result);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    private void appendElement(Element labelElem, LabelNode labelNode) {
+        for (LabelNode childNode : labelNode.getChildren()) {
+            Element newLabelElem = labelsXMLDoc.createElement("label");
+            newLabelElem.setAttribute("name", childNode.getName());
+            appendElement(newLabelElem, childNode);
+            labelElem.appendChild(newLabelElem);
+        }
+    }
+
+    public enum Method {
+        Random,
+        Clustering,
+        BalancedClustering
+    }
+}
