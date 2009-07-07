30a1ebb8c804fde18a0439c0dc813c7392ef97c7
Eleftherios Spyromitros-Xioufis <espyromi@csd.auth.gr>
2009-07-07 22:10:44 +0000

    Phi coefficient calculation moved to this class
    git-svn-id: svn://svn.code.sf.net/p/mulan/code/trunk/mulan@318 90ebec12-01d7-49c3-8feb-d42dc2bd4ece

--- src/mulan/core/data/Statistics.java
+++ src/mulan/core/data/Statistics.java
@@ -22,11 +22,15 @@ package mulan.core.data;
 
 import mulan.core.*;
 import java.io.Serializable;
+import java.text.DecimalFormat;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Set;
 import mulan.core.data.MultiLabelInstances;
 import weka.core.Instance;
 import weka.core.Instances;
+import weka.filters.Filter;
+import weka.filters.unsupervised.attribute.Remove;
 
 /**
  <!-- globalinfo-start -->
@@ -105,6 +109,8 @@ public class Statistics implements Serializable
     /** labelsets and their frequency */
     private HashMap<LabelSet,Integer> labelsets;
     
+	/** the array holding the phi correlations*/
+	double[][] phi;
         
     public Statistics() { }
     
@@ -207,6 +213,142 @@ public class Statistics implements Serializable
             examplesPerLabel[j] /= numInstances;        
     }
     
+    public double[][] calculatePhi(MultiLabelInstances dataSet) throws Exception {
+
+        numLabels = dataSet.getNumLabels();
+    	
+        /** the indices of the label attributes */
+    	int [] labelIndices;
+    	
+    	labelIndices = dataSet.getLabelIndices();
+    	numLabels = dataSet.getNumLabels();
+    	phi = new double[numLabels][numLabels];
+    	
+		Remove remove = new Remove();
+		remove.setInvertSelection(true);
+		remove.setAttributeIndicesArray(labelIndices);
+		remove.setInputFormat(dataSet.getDataSet());
+		Instances result = Filter.useFilter(dataSet.getDataSet(), remove);
+		result.setClassIndex(result.numAttributes() - 1);
+
+		for (int i = 0; i < numLabels; i++) {
+			
+			int a[] = new int[numLabels];
+			int b[] = new int[numLabels];
+			int c[] = new int[numLabels];
+			int d[] = new int[numLabels];
+			double e[] = new double[numLabels];
+			double f[] = new double[numLabels];
+			double g[] = new double[numLabels];
+			double h[] = new double[numLabels];
+			
+			for (int j = 0; j < result.numInstances(); j++) {
+				for (int l = 0; l < numLabels; l++) {
+					if (result.instance(j).value(i) == 0.0) {
+						if (result.instance(j).value(l) == 0.0) {
+							a[l]++;
+						} else {
+							c[l]++;
+						}
+					} else {
+						if (result.instance(j).value(l) == 0.0) {
+							b[l]++;
+						} else {
+							d[l]++;
+						}
+					}
+				}
+			}
+			for (int l = 0; l < numLabels; l++) {
+				e[l] = a[l] + b[l];
+				f[l] = c[l] + d[l];
+				g[l] = a[l] + c[l];
+				h[l] = b[l] + d[l];
+
+				double mult = e[l] * f[l] * g[l] * h[l];
+				double denominator = Math.sqrt(mult);
+				double nominator = a[l] * d[l] - b[l] * c[l];
+				phi[i][l] = nominator / denominator;
+
+			}
+		}
+		return phi;
+	}
+    
+	public void printPhiCorrelations() {
+		String pattern = "0.00";
+		DecimalFormat myFormatter = new DecimalFormat(pattern);
+
+		for (int i = 0; i < numLabels; i++) {
+			for (int j = 0; j < numLabels; j++) {
+				System.out.print(myFormatter.format(phi[i][j]) + " ");
+			}
+			System.out.println("");
+		}
+	}
+	
+	public double[] getPhiHistogram() {
+		double[] pairs = new double[numLabels * (numLabels - 1) / 2];
+		int counter = 0;
+		for (int i = 0; i < numLabels - 1; i++)
+			for (int j = i + 1; j < numLabels; j++) {
+				pairs[counter] = phi[i][j];
+				counter++;
+			}
+		return pairs;
+	}
+	
+	/**
+	 * returns the indices of the labels whose phi coefficient values lie
+	 * between -bound <= phi <= bound
+	 * 
+	 * @param labelIndex
+	 * @param bound
+	 * @return
+	 */
+	public int[] uncorrelatedLabels(int labelIndex, double bound) {
+		ArrayList<Integer> indiceslist = new ArrayList<Integer>();
+		for (int i = 0; i < numLabels; i++) {
+			if (Math.abs(phi[labelIndex][i]) <= bound) {
+				indiceslist.add(i);
+			}
+		}
+		int[] indices = new int[indiceslist.size()];
+		for (int i = 0; i < indiceslist.size(); i++) {
+			indices[i] = indiceslist.get(i);
+		}
+
+		return indices;
+	}
+	
+	/**
+	 * This method prints data, useful for the visualization of Phi per dataset.
+	 * It prints int(1/step) + 1 pairs of values. The first value of each pair
+	 * is the phi value and the second is the average number of labels that
+	 * correlate to the rest of the labels with correlation higher than the
+	 * specified phi value;
+	 * 
+	 * @param step
+	 *            the phi value increment step
+	 */
+	public void printPhiDiagram(double step) {
+		String pattern = "0.00";
+		DecimalFormat myFormatter = new DecimalFormat(pattern);
+
+		System.out.println("Phi      AvgCorrelated");
+		double phi = 0;
+		while (phi <= 1.001) {
+			double avgCorrelated = 0;
+			for (int i = 0; i < numLabels; i++) {
+				int[] temp = uncorrelatedLabels(i, phi);
+				avgCorrelated += (numLabels - temp.length);
+			}
+			avgCorrelated /= numLabels;
+			System.out.println(myFormatter.format(phi) + "     "
+					+ avgCorrelated);
+			phi += step;
+		}
+	}
     
     /** 
      * returns various multilabel statistics in textual representation 
