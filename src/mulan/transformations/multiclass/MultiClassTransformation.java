a449d92ad9325f63068fcb97c43985ce3dc10e50
Grigorios Tsoumakas <greg@csd.auth.gr>
2009-08-07 08:59:48 +0000

    Fixed missing Javadoc Updated some classes according to labelIndices Some minor changes
    git-svn-id: svn://svn.code.sf.net/p/mulan/code/trunk/mulan@337 90ebec12-01d7-49c3-8feb-d42dc2bd4ece

--- src/mulan/transformations/multiclass/MultiClassTransformation.java
+++ src/mulan/transformations/multiclass/MultiClassTransformation.java
@@ -1,20 +1,43 @@
+/*
+ *    This program is free software; you can redistribute it and/or modify
+ *    it under the terms of the GNU General Public License as published by
+ *    the Free Software Foundation; either version 2 of the License, or
+ *    (at your option) any later version.
+ *
+ *    This program is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *    GNU General Public License for more details.
+ *
+ *    You should have received a copy of the GNU General Public License
+ *    along with this program; if not, write to the Free Software
+ *    Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+ */
+
+/*
+ *    MultiClassTransformation.java
+ *    Copyright (C) 2009 Aristotle University of Thessaloniki, Thessaloniki, Greece
+ */
 
 package mulan.transformations.multiclass;
 
-import java.util.List;
-
 import mulan.core.data.MultiLabelInstances;
-import weka.core.Instance;
 import weka.core.Instances;
 
 /**
- * The interface for multi-class transformations methods.
+ * The interface for single-label multi-class transformations.
  * 
  * @author Stavros
  */
 public interface MultiClassTransformation {
 
+    /**
+     * Transforms a multi-label dataset to a multi-class single label dataset
+     * 
+     * @param dataSet a multi-label dataset
+     * @return a single-label multi-class dataset
+     * @throws java.lang.Exception
+     */
     public Instances transformInstances(MultiLabelInstances dataSet) throws Exception;
      
-    public List<Instance> transformInstance(Instance instance);
 }
