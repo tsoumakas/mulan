04aca116acbf7b77f87be7e7b34b7246384457cb
Grigorios Tsoumakas <greg@csd.auth.gr>
2010-02-06 23:05:55 +0000

    header update (last part)
    git-svn-id: svn://svn.code.sf.net/p/mulan/code/trunk/mulan@415 90ebec12-01d7-49c3-8feb-d42dc2bd4ece

--- src/mulan/classifier/neural/MMPRandomizedUpdateRule.java
+++ src/mulan/classifier/neural/MMPRandomizedUpdateRule.java
@@ -1,25 +1,23 @@
 /*
-*    This program is free software; you can redistribute it and/or modify
-*    it under the terms of the GNU General Public License as published by
-*    the Free Software Foundation; either version 2 of the License, or
-*    (at your option) any later version.
-*
-*    This program is distributed in the hope that it will be useful,
-*    but WITHOUT ANY WARRANTY; without even the implied warranty of
-*    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-*    GNU General Public License for more details.
-*
-*    You should have received a copy of the GNU General Public License
-*    along with this program; if not, write to the Free Software
-*    Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
-*/
+ *    This program is free software; you can redistribute it and/or modify
+ *    it under the terms of the GNU General Public License as published by
+ *    the Free Software Foundation; either version 2 of the License, or
+ *    (at your option) any later version.
+ *
+ *    This program is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *    GNU General Public License for more details.
+ *
+ *    You should have received a copy of the GNU General Public License
+ *    along with this program; if not, write to the Free Software
+ *    Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+ */
 
 /*
-*    MMPUniformUpdateRule.java
-*    Copyright (C) 2009 Aristotle University of Thessaloniki, Thessaloniki, Greece
-*
-*/
-
+ *    MMPRandomizedUpdateRule.java
+ *    Copyright (C) 2009-2010 Aristotle University of Thessaloniki, Thessaloniki, Greece
+ */
 package mulan.classifier.neural;
 
 import java.util.HashMap;
@@ -45,56 +43,56 @@ import mulan.evaluation.measure.Measure;
  */
 public class MMPRandomizedUpdateRule extends MMPUpdateRuleBase {
 
-	/**
-	 * Creates a new instance of {@link MMPRandomizedUpdateRule}.
-	 * 
-	 * @param perceptrons the list of perceptrons, representing the model, which will receive updates.
-	 * @param lossMeasure the loss measure used to decide when the model should be updated by the rule
-	 */
-	public MMPRandomizedUpdateRule(List<Neuron> perceptrons, Measure lossMeasure) {
-		super(perceptrons, lossMeasure);
-	}
+    /**
+     * Creates a new instance of {@link MMPRandomizedUpdateRule}.
+     *
+     * @param perceptrons the list of perceptrons, representing the model, which will receive updates.
+     * @param lossMeasure the loss measure used to decide when the model should be updated by the rule
+     */
+    public MMPRandomizedUpdateRule(List<Neuron> perceptrons, Measure lossMeasure) {
+        super(perceptrons, lossMeasure);
+    }
 
-	@Override
-	protected double[] computeUpdateParameters(DataPair example, double[] confidences, double loss) {
-		int numLabels = example.getOutput().length;
-		
-		boolean[] trueOutput = example.getOutputBoolean();
-		
-		Set<Integer> relevant = new HashSet<Integer>();
-		Set<Integer> irrelevant = new HashSet<Integer>();
-		for(int index = 0; index < numLabels; index++){
-			if(trueOutput[index]){
-				relevant.add(index);
-			} else {
-				irrelevant.add(index);
-			}
-		}
-		
-		// discover wrongly ordered pairs of labels and assign a random weight. 
-		//	we keep collecting the sum as it will be later used for normalization
-		double weightsSum = 0;
-		Map<int[],Double> weightedErrorSet = new HashMap<int[], Double>();
-		Random rnd = new Random();
-		for(int rLabel : relevant){
-			for(int irLabel : irrelevant){
-				if(confidences[rLabel] <= confidences[irLabel]){
-					double weight = rnd.nextDouble();
-					weightsSum += weight;
-					weightedErrorSet.put(new int[] { rLabel, irLabel }, weight);
-				}
-			}
-		}
-		
-		// normalize weights so they all sum to 1 and compute update parameters for perceptrons
-		double[] params = new double[numLabels];
-		Set<int[]> labelPairs = weightedErrorSet.keySet();
-		for(int[] pair : labelPairs){
-			double weight = weightedErrorSet.get(pair);
-			params[pair[0]] += weight * loss;
-			params[pair[1]] -= weight * loss;
-		}
-		
-		return params;
-	}
+    @Override
+    protected double[] computeUpdateParameters(DataPair example, double[] confidences, double loss) {
+        int numLabels = example.getOutput().length;
+
+        boolean[] trueOutput = example.getOutputBoolean();
+
+        Set<Integer> relevant = new HashSet<Integer>();
+        Set<Integer> irrelevant = new HashSet<Integer>();
+        for (int index = 0; index < numLabels; index++) {
+            if (trueOutput[index]) {
+                relevant.add(index);
+            } else {
+                irrelevant.add(index);
+            }
+        }
+
+        // discover wrongly ordered pairs of labels and assign a random weight.
+        //	we keep collecting the sum as it will be later used for normalization
+        double weightsSum = 0;
+        Map<int[], Double> weightedErrorSet = new HashMap<int[], Double>();
+        Random rnd = new Random();
+        for (int rLabel : relevant) {
+            for (int irLabel : irrelevant) {
+                if (confidences[rLabel] <= confidences[irLabel]) {
+                    double weight = rnd.nextDouble();
+                    weightsSum += weight;
+                    weightedErrorSet.put(new int[]{rLabel, irLabel}, weight);
+                }
+            }
+        }
+
+        // normalize weights so they all sum to 1 and compute update parameters for perceptrons
+        double[] params = new double[numLabels];
+        Set<int[]> labelPairs = weightedErrorSet.keySet();
+        for (int[] pair : labelPairs) {
+            double weight = weightedErrorSet.get(pair);
+            params[pair[0]] += weight * loss;
+            params[pair[1]] -= weight * loss;
+        }
+
+        return params;
+    }
 }
--- src/mulan/classifier/neural/MMPUpdateRuleType.java
+++ src/mulan/classifier/neural/MMPUpdateRuleType.java
@@ -1,27 +1,24 @@
 /*
-*    This program is free software; you can redistribute it and/or modify
-*    it under the terms of the GNU General Public License as published by
-*    the Free Software Foundation; either version 2 of the License, or
-*    (at your option) any later version.
-*
-*    This program is distributed in the hope that it will be useful,
-*    but WITHOUT ANY WARRANTY; without even the implied warranty of
-*    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-*    GNU General Public License for more details.
-*
-*    You should have received a copy of the GNU General Public License
-*    along with this program; if not, write to the Free Software
-*    Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
-*/
+ *    This program is free software; you can redistribute it and/or modify
+ *    it under the terms of the GNU General Public License as published by
+ *    the Free Software Foundation; either version 2 of the License, or
+ *    (at your option) any later version.
+ *
+ *    This program is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *    GNU General Public License for more details.
+ *
+ *    You should have received a copy of the GNU General Public License
+ *    along with this program; if not, write to the Free Software
+ *    Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+ */
 
 /*
-*    MMPUpdateRuleType.java
-*    Copyright (C) 2009 Aristotle University of Thessaloniki, Thessaloniki, Greece
-*
-*/
-
+ *    MMPUpdateRuleType.java
+ *    Copyright (C) 2009-2010 Aristotle University of Thessaloniki, Thessaloniki, Greece
+ */
 package mulan.classifier.neural;
-
 
 /**
  * The enumeration of update rules, which can be used by the 
@@ -30,10 +27,11 @@ package mulan.classifier.neural;
  * @author Jozef Vilcek
  */
 public enum MMPUpdateRuleType {
-	/** Indicates usage of uniform update rule */
-	UniformUpdate,
-	/** Indicates usage of maximum update rule */
-	MaxUpdate,
-	/** Indicates usage of randomized update rule */
-	RandomizedUpdate,
+
+    /** Indicates usage of uniform update rule */
+    UniformUpdate,
+    /** Indicates usage of maximum update rule */
+    MaxUpdate,
+    /** Indicates usage of randomized update rule */
+    RandomizedUpdate,
 }
