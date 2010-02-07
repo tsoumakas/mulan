3eb081f1f601f98dedab61c747f5e15cd1c45076
Grigorios Tsoumakas <greg@csd.auth.gr>
2010-02-07 19:28:14 +0000

    - Rewrite of the class so that it does not require writing an intermediary file, something that was causing a bug
    git-svn-id: svn://svn.code.sf.net/p/mulan/code/trunk@416 90ebec12-01d7-49c3-8feb-d42dc2bd4ece

--- mulan/src/mulan/data/ConverterCLUS.java
+++ mulan/src/mulan/data/ConverterCLUS.java
@@ -16,61 +16,33 @@
 
 /*
  *    ConverterCLUS.java
- *    Copyright (C) 2009 Aristotle University of Thessaloniki, Thessaloniki, Greece
+ *    Copyright (C) 2009-2010 Aristotle University of Thessaloniki, Thessaloniki, Greece
  */
 package mulan.data;
 
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
-import java.io.File;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.Map;
-import javax.xml.parsers.DocumentBuilder;
-import javax.xml.parsers.DocumentBuilderFactory;
-import javax.xml.transform.OutputKeys;
-import javax.xml.transform.Source;
-import javax.xml.transform.Transformer;
-import javax.xml.transform.TransformerFactory;
-import javax.xml.transform.dom.DOMSource;
-import javax.xml.transform.stream.StreamResult;
-import org.w3c.dom.Document;
-import org.w3c.dom.Element;
 import weka.core.Attribute;
 import weka.core.FastVector;
 import weka.core.Instance;
 import weka.core.Instances;
-import weka.core.converters.ArffSaver;
 
 /**
- * Class that converts the datasets in CLUS format to Mulan compatible dataset.
- * An arff and xml file are created. The arff file contains the original
- * dataset with all labels converted to separate attributes and properly
- * converted instances. The xml file contains the hierarchy of the labels.
+ * <p>Class that converts a dataset that is originally in the format of the
+ * <a href="http://www.cs.kuleuven.be/~dtai/clus/">Clus system</a> to a format
+ * that is suitable for Mulan. An arff and an xml file are created.</p>
+ * <p>The arff file contains the original dataset with all labels converted to
+ * separate attributes and properly converted instances. The xml file contains
+ * the hierarchy of the labels.</p>
  *
  * @author George Saridis
+ * @author Grigorios Tsoumakas
  */
 public class ConverterCLUS {
-
-    private static String[] hierarchicalAtts = null;
-    private static ArrayList<String> labelVals = null;
-
-    /**
-     * Converts the original dataset to mulan compatible dataset.The output
-     * files will be named "converted " + source's file name.
-     *
-     * @param sourceFileName the sourceFileName
-     * @throws java.lang.Exception
-     */
-    public static void convert(String sourceFileName) throws Exception {
-        String arffName = "converted " + sourceFileName;
-        String xmlName = "converted " + sourceFileName.split(".arff", 2)[0] + ".xml";
-        convert(sourceFileName, arffName, xmlName);
-    }
 
     /**
      * Converts the original dataset to mulan compatible dataset.
@@ -80,172 +52,152 @@ public class ConverterCLUS {
      * @param xmlName the xml name
      * @throws java.lang.Exception
      */
-    public static void convert(String sourceFileName, String arffName, String xmlName) throws Exception {
-        Instances dataSet = readDataSet(sourceFileName,arffName);
-        Instances convertedDataSet = convertInstances(dataSet);
-        saveDataSet(arffName, convertedDataSet);
-        LabelsMetaData labelsMetaData = createLabelsMetaData();
-        Document labelsXMLDoc = createXMLFile(labelsMetaData);
-        saveToXMLFile(xmlName, labelsXMLDoc);
-    }
-
-    private static Instances readDataSet(String sourceFileName,String arffName) {
+    public static void convert(String sourceFilename, String arffFilename, String xmlFilename) throws Exception {
         String line;
-        Instances dataSet = null;
         try {
-            File sourceFile = new File(sourceFileName);
-            FileReader frInput = new FileReader(sourceFile);
-            BufferedReader brInput = new BufferedReader(frInput);
-            
-            BufferedWriter convertedFile = new BufferedWriter(new FileWriter(arffName));
-            while (!(line = brInput.readLine()).contains("class")) {
-                convertedFile.write(line + "\n");
-            }
+            BufferedReader brInput = new BufferedReader(new FileReader(sourceFilename));
 
-            line = line.split("hierarchical ", 2)[1];
-            hierarchicalAtts = line.split(",");
-
-            brInput.readLine(); // skip the blank line
-            convertedFile.write(brInput.readLine() + "\n");
-            String[] data;
-            labelVals = new ArrayList();
-
+            String relationName = null;
+            FastVector attInfo = new FastVector();
+            Instances data = null;
+            int numAttributes = 0;
+            String[] labelNames = null;
             while ((line = brInput.readLine()) != null) {
-                data = line.split(",");
-                for (int i = 0; i < data.length - 1; i++) {
-                    convertedFile.write(data[i]);
-                    if (i < (data.length - 2)) {
-                        convertedFile.write(",");
+                if (line.startsWith("@RELATION")) {
+                    relationName = line.replace("@RELATION ", "").replaceAll("'", "").trim();
+                    continue;
+                }
+                if (line.startsWith("@ATTRIBUTE ")) {
+                    String tokens[] = line.split("\\s+");
+                    Attribute att;
+                    if (line.startsWith("@ATTRIBUTE class")) {
+                        labelNames = tokens[3].split(",");
+                        for (int i = 0; i < labelNames.length; i++) {
+                            FastVector labelValues = new FastVector();
+                            labelValues.addElement("0");
+                            labelValues.addElement("1");
+                            att = new Attribute(labelNames[i], labelValues);
+                            attInfo.addElement(att);
+                        }
+                    } else {
+                        numAttributes++;
+                        if (tokens[2].equals("numeric")) {
+                            att = new Attribute(tokens[1]);
+                        } else {
+                            FastVector nominalValues = new FastVector();
+                            tokens[2].substring(1, tokens[2].length() - 1);
+                            String[] nominalTokens = tokens[2].substring(1, tokens[2].length() - 1).split(",");
+                            for (int i = 0; i < nominalTokens.length; i++) {
+                                nominalValues.addElement(nominalTokens[i]);
+                            }
+                            att = new Attribute(tokens[1], nominalValues);
+                        }
+                        attInfo.addElement(att);
+                    }
+                    continue;
+                }
+                if (line.toLowerCase().startsWith("@data")) {
+                    data = new Instances(relationName, attInfo, 0);
+                    while ((line = brInput.readLine()) != null) {
+                        // fill data
+                        String[] tokens = line.split(",");
+                        double[] values = new double[attInfo.size()];
+                        for (int i = 0; i < numAttributes; i++) {
+                            Attribute att = (Attribute) attInfo.elementAt(i);
+                            if (att.isNumeric()) {
+                                values[i] = Double.parseDouble(tokens[i]);
+                            } else {
+                                values[i] = att.indexOfValue(tokens[i]);
+                            }
+                        }
+                        String[] labels = tokens[numAttributes].split("@");
+                        // fill class values
+                        for (int j = 0; j < labels.length; j++) {
+                            String[] splitedLabels = labels[j].split("/");
+                            String attrName = splitedLabels[0];
+                            Attribute att = data.attribute(attrName);
+                            values[attInfo.indexOf(att)] = 1;
+                            for (int k = 1; k < splitedLabels.length; k++) {
+                                attrName = attrName + "/" + splitedLabels[k];
+                                att = data.attribute(attrName);
+                                values[attInfo.indexOf(att)] = 1;
+                            }
+                        }
+                        Instance instance = new Instance(1, values);
+                        data.add(instance);
                     }
                 }
-                convertedFile.write("\n");
-                labelVals.add(data[data.length - 1]);
-
             }
-            convertedFile.close();
-            dataSet = new Instances(new FileReader(arffName));
+            BufferedWriter writer;
+            writer = new BufferedWriter(new FileWriter(arffFilename));
+            writer.write(data.toString());
+            writer.close();
+
+            // write xml file
+            writer = new BufferedWriter(new FileWriter(xmlFilename));
+            writer.write("<?xml version=\"1.0\" encoding=\"UTF-8\" standalone=\"no\"?>\n");
+            writer.write("<labels xmlns=\"http://mulan.sourceforge.net/labels\">\n");
+            writer.write("<label name=\"" + labelNames[0] + "\">");
+            int depth = 0;
+            for (int i = 1; i < labelNames.length; i++) {
+                int difSlashes = countSlashes(labelNames[i]) - countSlashes(labelNames[i - 1]);
+                // child
+                if (difSlashes == 1) {
+                    depth++;
+                    writer.write("\n");
+                    for (int j = 0; j < depth; j++) {
+                        writer.write("\t");
+                    }
+                    writer.write("<label name=\"" + labelNames[i] + "\">");
+                }
+                // sibling
+                if (difSlashes == 0) {
+                    writer.write("</label>\n");
+                    for (int j = 0; j < depth; j++) {
+                        writer.write("\t");
+                    }
+                    writer.write("<label name=\"" + labelNames[i] + "\">");
+                }
+                // ancestor
+                if (difSlashes < 0) {
+                    writer.write("</label>\n");
+                    for (int j = 0; j < Math.abs(difSlashes); j++) {
+                        depth--;
+                        for (int k = 0; k < depth; k++) {
+                            writer.write("\t");
+                        }
+                        writer.write("</label>\n");
+                    }
+                    for (int j = 0; j < depth; j++) {
+                        writer.write("\t");
+                    }
+                    writer.write("<label name=\"" + labelNames[i] + "\">");
+                }
+            }
+            writer.write("</label>\n");
+            while (depth > 0) {
+                for (int k = 0; k < depth; k++) {
+                    writer.write("\t");
+                }
+                writer.write("</label>\n");
+                depth--;
+            }
+            writer.write("</labels>");
+            writer.close();
+
+
         } catch (IOException ioEx) {
             ioEx.printStackTrace();
         }
-        return dataSet;
     }
 
-    private static Instances convertInstances(Instances dataSet) {
-        FastVector atts = new FastVector();
-        for (int i = 0; i < dataSet.numAttributes(); i++) {
-            atts.addElement(dataSet.attribute(i));
-        }
-
-        FastVector labelValues = new FastVector();
-        labelValues.addElement("0");
-        labelValues.addElement("1");
-
-        for (int i = 0; i < hierarchicalAtts.length; i++) {
-            atts.addElement(new Attribute(hierarchicalAtts[i], labelValues));
-        }
-
-        Instances convertedDataSet = new Instances(dataSet.relationName(), atts, dataSet.numInstances());
-
-        for (int i = 0; i < dataSet.numInstances(); i++) {
-            double[] newValues = new double[convertedDataSet.numAttributes()];
-            Arrays.fill(newValues, 0);
-
-            double[] values = dataSet.instance(i).toDoubleArray();
-            System.arraycopy(values, 0, newValues, 0, values.length);
-
-            String[] labels = labelVals.get(i).split("@");
-
-            for (int j = 0; j < labels.length; j++) {
-                String[] splitedLabels = labels[j].split("/");
-                String attr = splitedLabels[0];
-                Attribute at = convertedDataSet.attribute(attr);
-                newValues[atts.indexOf(at)] = 1;
-                for (int k = 1; k < splitedLabels.length; k++) {
-                    attr = attr + "/" + splitedLabels[k];
-                    at = convertedDataSet.attribute(attr);
-                    newValues[atts.indexOf(at)] = 1;
-                }
-            }
-            convertedDataSet.add(new Instance(dataSet.instance(i).weight(), newValues));
-        }
-
-        return convertedDataSet;
-    }
-
-    private static LabelsMetaDataImpl createLabelsMetaData() {
-        LabelsMetaDataImpl labelsMetaData = new LabelsMetaDataImpl();
-        ArrayList<LabelNodeImpl> rootLabels = new ArrayList();
-        Map<String, LabelNodeImpl> allLabelNodes = new HashMap<String, LabelNodeImpl>();
-        for (int i = 0; i < hierarchicalAtts.length; i++) {
-            String hierarchicalLabels = hierarchicalAtts[i];
-            int idx = hierarchicalLabels.lastIndexOf("/");
-            if (idx != -1) {
-                String newLabel = hierarchicalLabels.substring(idx + 1);
-                String parentLabel = hierarchicalLabels.substring(0, idx);
-
-                LabelNodeImpl label = new LabelNodeImpl(parentLabel + "/" + newLabel);
-                allLabelNodes.put(parentLabel + "/" + newLabel, label);
-
-                LabelNodeImpl parent = allLabelNodes.get(parentLabel);
-                parent.addChildNode(label);
-            } else {
-                LabelNodeImpl rootNode = new LabelNodeImpl(hierarchicalLabels);
-                rootLabels.add(rootNode);
-                allLabelNodes.put(hierarchicalLabels, rootNode);
+    private static int countSlashes(String label) {
+        int counter = 0;
+        for (int i = 0; i < label.length(); i++) {
+            if (label.charAt(i) == '/') {
+                counter++;
             }
         }
-        for (int i = 0; i < rootLabels.size(); i++) {
-            labelsMetaData.addRootNode(rootLabels.get(i));
-        }
-        return labelsMetaData;
-    }
-
-    private static Document createXMLFile(LabelsMetaData metaData) throws Exception {
-        DocumentBuilderFactory docBF = DocumentBuilderFactory.newInstance();
-        DocumentBuilder docBuilder = docBF.newDocumentBuilder();
-        Document labelsXMLDoc = docBuilder.newDocument();
-
-        Element rootElement = labelsXMLDoc.createElement("labels");
-        rootElement.setAttribute("xmlns", "http://mulan.sourceforge.net/labels");
-        labelsXMLDoc.appendChild(rootElement);
-        for (LabelNode rootLabel : metaData.getRootLabels()) {
-            Element newLabelElem = labelsXMLDoc.createElement("label");
-            newLabelElem.setAttribute("name", rootLabel.getName());
-            appendElement(newLabelElem, rootLabel, labelsXMLDoc);
-            rootElement.appendChild(newLabelElem);
-        }
-        return labelsXMLDoc;
-    }
-
-    private static void appendElement(Element labelElem, LabelNode labelNode, Document labelsXMLDoc) {
-        for (LabelNode childNode : labelNode.getChildren()) {
-            Element newLabelElem = labelsXMLDoc.createElement("label");
-            newLabelElem.setAttribute("name", childNode.getName());
-            appendElement(newLabelElem, childNode, labelsXMLDoc);
-            labelElem.appendChild(newLabelElem);
-        }
-    }
-
-    private static void saveToXMLFile(String xmlName, Document labelsXMLDoc) {
-        Source source = new DOMSource(labelsXMLDoc);
-        File xmlFile = new File(xmlName);
-        StreamResult result = new StreamResult(xmlFile);
-        try {
-            Transformer transformer = TransformerFactory.newInstance().newTransformer();
-            transformer.setOutputProperty(OutputKeys.INDENT, "yes");
-            transformer.setOutputProperty("{http://xml.apache.org/xslt}indent-amount", "4");
-            transformer.setOutputProperty(OutputKeys.METHOD, "xml");
-            transformer.transform(source, result);
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-
-    }
-
-    private static void saveDataSet(String arffName, Instances dataSet) throws IOException {
-        ArffSaver saver = new ArffSaver();
-        saver.setInstances(dataSet);
-        saver.setFile(new File(arffName));
-        saver.writeBatch();
+        return counter;
     }
 }
