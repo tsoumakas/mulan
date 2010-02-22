81cb36732d469198379ab0c6ae15eaa0e5e96f80
Grigorios Tsoumakas <greg@csd.auth.gr>
2010-02-22 07:26:19 +0000

    code auto-format
    git-svn-id: svn://svn.code.sf.net/p/mulan/code/trunk/mulan@430 90ebec12-01d7-49c3-8feb-d42dc2bd4ece

--- test/mulan/classifier/MultiLabelLearnerTestBase.java
+++ test/mulan/classifier/MultiLabelLearnerTestBase.java
@@ -35,107 +35,106 @@ import org.junit.Test;
 @Ignore
 public abstract class MultiLabelLearnerTestBase {
 
-	/** General dataset which should be handled by all multi-label learners. Used to check learner in black-box mode */
-	protected static final DataSetDefinition DATA_SET;
+    /** General dataset which should be handled by all multi-label learners. Used to check learner in black-box mode */
+    protected static final DataSetDefinition DATA_SET;
 
-	/**
-	 * Gets a learner instance which can be tested in each test case
-	 * @return
-	 */
-	protected abstract MultiLabelLearnerBase getLearner();
-		
-	static {
-		DATA_SET = new DataSetDefinition("GeneralDataSet");
-		DATA_SET.addAttribute(Attribute.createNumericAttribute("feature_1"));
-		DATA_SET.addAttribute(Attribute.createNumericAttribute("feature_2"));
-		DATA_SET.addAttribute(Attribute.createNominalAttribute("feature_3", new String[]{"n1", "n2", "n3", "n4"}));
-		DATA_SET.addAttribute(Attribute.createLabelAttribute("label_1"));
-		DATA_SET.addAttribute(Attribute.createLabelAttribute("label_2"));
-		DATA_SET.addAttribute(Attribute.createLabelAttribute("label_3"));
-		DATA_SET.addAttribute(Attribute.createLabelAttribute("label_4"));
-		DATA_SET.setExamplesCount(100);
-	}
-	
-	
-	@Test
-	public void testGetTechnicalInformation(){
-		Assert.assertNotNull(getLearner().getTechnicalInformation());
-	}
-	
-	@Test
-	public void testMakeCopy() throws Exception{
-		MultiLabelLearnerBase learner = getLearner();
-		MultiLabelLearnerBase copy = (MultiLabelLearnerBase) learner.makeCopy();
-		
-		// rough checks on obvious parameters ... interesting would be to compare learned models
-		Assert.assertEquals(learner.numLabels, copy.numLabels);
-		Assert.assertTrue(Arrays.equals(learner.featureIndices, copy.featureIndices));
-		Assert.assertTrue(Arrays.equals(learner.labelIndices, copy.labelIndices));
-	
-		// build on copy
-		MultiLabelInstances mlDataSet = DataSetBuilder.CreateDataSet(DATA_SET);
-		copy.build(mlDataSet);
-		
-		// create a copy and test prediction - if build learner knowledge is persisted
-		copy = (MultiLabelLearnerBase) copy.makeCopy();
-		MultiLabelOutput prediction = copy.makePrediction(mlDataSet.getDataSet().firstInstance());
-		// Note: precision is not tested - if original performs same as copy
-		Assert.assertNotNull(prediction);
-	}
-	
-	@Test(expected=ArgumentNullException.class)
-	public void testBuild_WithNullDataSet() throws Exception{
-		getLearner().build(null);
-	}
-	
-	@Test
-	public void testBuild_WithMissingValues() throws Exception{
-		DataSetDefinition definition = new DataSetDefinition("MissingValuesDataSet");
-		definition.addAttribute(Attribute.createNumericAttribute("feature_1").setMissingValuesProbability(0.3));
-		definition.addAttribute(Attribute.createNominalAttribute("feature_3", new String[]{"n1", "n2", "n3", "n4"}));
-		definition.addAttribute(Attribute.createLabelAttribute("label_1"));
-		definition.addAttribute(Attribute.createLabelAttribute("label_2"));
-		
-		MultiLabelInstances mlDataSet = DataSetBuilder.CreateDataSet(definition);
-		getLearner().build(mlDataSet);
-	}
-	
-	@Test
-	public void testBuild() throws Exception {
-		MultiLabelInstances mlDataSet = DataSetBuilder.CreateDataSet(DATA_SET);
-		MultiLabelLearnerBase learner = getLearner();
-		
-		Assert.assertNull(learner.labelIndices);
-		Assert.assertNull(learner.featureIndices);
-		Assert.assertEquals(0, learner.numLabels);
-		
-		learner.build(mlDataSet);
-		
-		Assert.assertNotNull(learner.labelIndices);
-		Assert.assertNotNull(learner.featureIndices);
-		Assert.assertEquals(DATA_SET.getLabelsCount(), learner.numLabels);
-		
-	}
-	
-	@Test(expected=ArgumentNullException.class)
-	public void testMakePrediction_WithNullData() throws Exception {
-		getLearner().makePrediction(null);
-	}
-	
-	@Test(expected=ModelInitializationException.class)
-	public void testMakePrediction_BeforeBuild() throws Exception {
-		MultiLabelInstances mlDataSet = DataSetBuilder.CreateDataSet(DATA_SET);
-		getLearner().makePrediction(mlDataSet.getDataSet().firstInstance());
-	}
-	
-	@Test()
-	public void testMakePrediction_Generic() throws Exception {
-		MultiLabelInstances mlDataSet = DataSetBuilder.CreateDataSet(DATA_SET);
-		MultiLabelLearnerBase learner = getLearner();
-		
-		learner.build(mlDataSet);
-		MultiLabelOutput out = learner.makePrediction(mlDataSet.getDataSet().firstInstance());
-		
-		Assert.assertNotNull(out);
-	}
+    /**
+     * Gets a learner instance which can be tested in each test case
+     * @return
+     */
+    protected abstract MultiLabelLearnerBase getLearner();
+
+    static {
+        DATA_SET = new DataSetDefinition("GeneralDataSet");
+        DATA_SET.addAttribute(Attribute.createNumericAttribute("feature_1"));
+        DATA_SET.addAttribute(Attribute.createNumericAttribute("feature_2"));
+        DATA_SET.addAttribute(Attribute.createNominalAttribute("feature_3", new String[]{"n1", "n2", "n3", "n4"}));
+        DATA_SET.addAttribute(Attribute.createLabelAttribute("label_1"));
+        DATA_SET.addAttribute(Attribute.createLabelAttribute("label_2"));
+        DATA_SET.addAttribute(Attribute.createLabelAttribute("label_3"));
+        DATA_SET.addAttribute(Attribute.createLabelAttribute("label_4"));
+        DATA_SET.setExamplesCount(100);
+    }
+
+    @Test
+    public void testGetTechnicalInformation() {
+        Assert.assertNotNull(getLearner().getTechnicalInformation());
+    }
+
+    @Test
+    public void testMakeCopy() throws Exception {
+        MultiLabelLearnerBase learner = getLearner();
+        MultiLabelLearnerBase copy = (MultiLabelLearnerBase) learner.makeCopy();
+
+        // rough checks on obvious parameters ... interesting would be to compare learned models
+        Assert.assertEquals(learner.numLabels, copy.numLabels);
+        Assert.assertTrue(Arrays.equals(learner.featureIndices, copy.featureIndices));
+        Assert.assertTrue(Arrays.equals(learner.labelIndices, copy.labelIndices));
+
+        // build on copy
+        MultiLabelInstances mlDataSet = DataSetBuilder.CreateDataSet(DATA_SET);
+        copy.build(mlDataSet);
+
+        // create a copy and test prediction - if build learner knowledge is persisted
+        copy = (MultiLabelLearnerBase) copy.makeCopy();
+        MultiLabelOutput prediction = copy.makePrediction(mlDataSet.getDataSet().firstInstance());
+        // Note: precision is not tested - if original performs same as copy
+        Assert.assertNotNull(prediction);
+    }
+
+    @Test(expected = ArgumentNullException.class)
+    public void testBuild_WithNullDataSet() throws Exception {
+        getLearner().build(null);
+    }
+
+    @Test
+    public void testBuild_WithMissingValues() throws Exception {
+        DataSetDefinition definition = new DataSetDefinition("MissingValuesDataSet");
+        definition.addAttribute(Attribute.createNumericAttribute("feature_1").setMissingValuesProbability(0.3));
+        definition.addAttribute(Attribute.createNominalAttribute("feature_3", new String[]{"n1", "n2", "n3", "n4"}));
+        definition.addAttribute(Attribute.createLabelAttribute("label_1"));
+        definition.addAttribute(Attribute.createLabelAttribute("label_2"));
+
+        MultiLabelInstances mlDataSet = DataSetBuilder.CreateDataSet(definition);
+        getLearner().build(mlDataSet);
+    }
+
+    @Test
+    public void testBuild() throws Exception {
+        MultiLabelInstances mlDataSet = DataSetBuilder.CreateDataSet(DATA_SET);
+        MultiLabelLearnerBase learner = getLearner();
+
+        Assert.assertNull(learner.labelIndices);
+        Assert.assertNull(learner.featureIndices);
+        Assert.assertEquals(0, learner.numLabels);
+
+        learner.build(mlDataSet);
+
+        Assert.assertNotNull(learner.labelIndices);
+        Assert.assertNotNull(learner.featureIndices);
+        Assert.assertEquals(DATA_SET.getLabelsCount(), learner.numLabels);
+
+    }
+
+    @Test(expected = ArgumentNullException.class)
+    public void testMakePrediction_WithNullData() throws Exception {
+        getLearner().makePrediction(null);
+    }
+
+    @Test(expected = ModelInitializationException.class)
+    public void testMakePrediction_BeforeBuild() throws Exception {
+        MultiLabelInstances mlDataSet = DataSetBuilder.CreateDataSet(DATA_SET);
+        getLearner().makePrediction(mlDataSet.getDataSet().firstInstance());
+    }
+
+    @Test()
+    public void testMakePrediction_Generic() throws Exception {
+        MultiLabelInstances mlDataSet = DataSetBuilder.CreateDataSet(DATA_SET);
+        MultiLabelLearnerBase learner = getLearner();
+
+        learner.build(mlDataSet);
+        MultiLabelOutput out = learner.makePrediction(mlDataSet.getDataSet().firstInstance());
+
+        Assert.assertNotNull(out);
+    }
 }
