a5443a5cbd24e2e45102c0ded38a98e64bce6b77
Eleftherios Spyromitros-Xioufis <espyromi@csd.auth.gr>
2010-03-10 13:16:05 +0000

    ignore abstract classes in tests
    git-svn-id: svn://svn.code.sf.net/p/mulan/code/trunk/mulan@448 90ebec12-01d7-49c3-8feb-d42dc2bd4ece

--- test/mulan/classifier/transformation/TransformationBasedMultiLabelLearnerTest.java
+++ test/mulan/classifier/transformation/TransformationBasedMultiLabelLearnerTest.java
@@ -1,10 +1,12 @@
 package mulan.classifier.transformation;
 
 import org.junit.Before;
+import org.junit.Ignore;
 
 import mulan.classifier.MultiLabelLearnerBase;
 import mulan.classifier.MultiLabelLearnerTestBase;
 
+@Ignore
 public abstract class TransformationBasedMultiLabelLearnerTest extends
 		MultiLabelLearnerTestBase {
 
