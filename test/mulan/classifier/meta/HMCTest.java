985070593fac423f6b4e635ddd2810d33eb1a057
Grigorios Tsoumakas <greg@csd.auth.gr>
2010-03-11 15:39:59 +0000

    minor changes
    git-svn-id: svn://svn.code.sf.net/p/mulan/code/trunk/mulan@467 90ebec12-01d7-49c3-8feb-d42dc2bd4ece

--- test/mulan/classifier/meta/HMCTest.java
+++ test/mulan/classifier/meta/HMCTest.java
@@ -1,12 +1,12 @@
 package mulan.classifier.meta;
 
-import mulan.classifier.lazy.BRkNN;
+import mulan.classifier.transformation.BinaryRelevance;
+import weka.classifiers.trees.J48;
 
 public class HMCTest extends MultiLabelMetaLearnerTest {
 
-	@Override
-	public void setUp() throws Exception {
-		learner = new  HMC(new BRkNN(10));
-	}
-
+    @Override
+    public void setUp() throws Exception {
+        learner = new HMC(new BinaryRelevance(new J48()));
+    }
 }
