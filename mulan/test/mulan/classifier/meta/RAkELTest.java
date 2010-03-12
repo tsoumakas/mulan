55e24c9aa680473bbec3645314f445b5aed42439
Grigorios Tsoumakas <greg@csd.auth.gr>
2010-03-12 09:55:25 +0000

    More appropriate RAkEL test
    git-svn-id: svn://svn.code.sf.net/p/mulan/code/trunk@471 90ebec12-01d7-49c3-8feb-d42dc2bd4ece

--- mulan/test/mulan/classifier/meta/RAkELTest.java
+++ mulan/test/mulan/classifier/meta/RAkELTest.java
@@ -1,12 +1,13 @@
 package mulan.classifier.meta;
 
-import mulan.classifier.lazy.BRkNN;
+import mulan.classifier.transformation.LabelPowerset;
+import weka.classifiers.trees.J48;
 
 public class RAkELTest extends MultiLabelMetaLearnerTest {
 
 	@Override
 	public void setUp() throws Exception {
-		learner = new RAkEL(new BRkNN(10));
+		learner = new RAkEL(new LabelPowerset(new J48()));
 	}
 
 }
