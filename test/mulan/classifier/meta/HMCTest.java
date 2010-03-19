15c36dd19bdf18a53290ac31360b5cf9a58b20ec
Grigorios Tsoumakas <greg@csd.auth.gr>
2010-03-19 12:03:43 +0000

    hierarchical10
    git-svn-id: svn://svn.code.sf.net/p/mulan/code/trunk/mulan@497 90ebec12-01d7-49c3-8feb-d42dc2bd4ece

--- test/mulan/classifier/meta/HMCTest.java
+++ test/mulan/classifier/meta/HMCTest.java
@@ -1,11 +1,15 @@
 package mulan.classifier.meta;
 
 import junit.framework.Assert;
+import mulan.classifier.MultiLabelLearner;
 import mulan.classifier.MultiLabelLearnerBase;
 import mulan.classifier.MultiLabelOutput;
 import mulan.classifier.transformation.BinaryRelevance;
 import mulan.data.MultiLabelInstances;
 import weka.classifiers.trees.J48;
+import weka.core.Instances;
+import weka.filters.Filter;
+import weka.filters.unsupervised.attribute.SwapValues;
 
 public class HMCTest extends MultiLabelMetaLearnerTest {
 
@@ -34,4 +38,34 @@ public class HMCTest extends MultiLabelMetaLearnerTest {
             Assert.assertEquals(mlo1, mlo2);
         }
     }
+
+
+    @Override
+    public void testBuildWith10ClassLabels() throws Exception {
+        String trainDatasetPath = path + "hierarchical-train.arff";
+        String testDatasetPath = path + "hierarchical-test.arff";
+        String xmlLabelsDefFilePath = path + "hierarchical.xml";
+        MultiLabelInstances trainDataSet = new MultiLabelInstances(
+                trainDatasetPath, xmlLabelsDefFilePath);
+        MultiLabelInstances testDataSet = new MultiLabelInstances(
+                testDatasetPath, xmlLabelsDefFilePath);
+        String trainDatasetPath2 = path + "hierarchical10-train.arff";
+        String testDatasetPath2 = path + "hierarchical10-test.arff";
+        MultiLabelInstances trainDataSet2 = new MultiLabelInstances(
+                trainDatasetPath2, xmlLabelsDefFilePath);
+        MultiLabelInstances testDataSet2 = new MultiLabelInstances(
+                testDatasetPath2, xmlLabelsDefFilePath);
+
+
+        MultiLabelLearner learner1 = getLearner();
+        MultiLabelLearner learner2 = learner1.makeCopy();
+        learner1.build(trainDataSet);
+        learner2.build(trainDataSet2);
+        for (int i = 0; i < testDataSet.getDataSet().numInstances(); i++) {
+            MultiLabelOutput mlo1 = learner1.makePrediction(testDataSet.getDataSet().instance(i));
+            MultiLabelOutput mlo2 = learner2.makePrediction(testDataSet2.getDataSet().instance(i));
+            Assert.assertEquals(mlo1, mlo2);
+        }
+    }
+
 }
