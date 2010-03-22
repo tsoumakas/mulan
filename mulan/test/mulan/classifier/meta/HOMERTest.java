e4ae6c46053e024cd27d18353f768f9ac8e5b872
Grigorios Tsoumakas <greg@csd.auth.gr>
2010-03-22 20:44:15 +0000

    new release
    git-svn-id: svn://svn.code.sf.net/p/mulan/code/trunk@500 90ebec12-01d7-49c3-8feb-d42dc2bd4ece

--- mulan/test/mulan/classifier/meta/HOMERTest.java
+++ mulan/test/mulan/classifier/meta/HOMERTest.java
@@ -1,12 +1,15 @@
 package mulan.classifier.meta;
 
-import mulan.classifier.lazy.BRkNN;
+import mulan.classifier.transformation.LabelPowerset;
+import org.junit.Ignore;
+import weka.classifiers.trees.J48;
 
+@Ignore
 public class HOMERTest extends MultiLabelMetaLearnerTest {
 
-	@Override
-	public void setUp() throws Exception {
-		learner = new HOMER(new BRkNN(10), 3, HierarchyBuilder.Method.Random);
-	}
-
+    @Override
+    public void setUp() throws Exception {
+        learner = new HOMER(new LabelPowerset(new J48()), 3, HierarchyBuilder.Method.Random);
+    }
+    
 }
