dd74f30426b7be1724569d85da717dc29fc2031b
Grigorios Tsoumakas <greg@csd.auth.gr>
2010-05-02 17:12:51 +0000

    Adding AUC measure (micro/macro)
    git-svn-id: svn://svn.code.sf.net/p/mulan/code/trunk/mulan@511 90ebec12-01d7-49c3-8feb-d42dc2bd4ece

---
+++ src/mulan/evaluation/measure/LabelBasedAUC.java
@@ -0,0 +1,81 @@
+/*
+ *    This program is free software; you can redistribute it and/or modify
+ *    it under the terms of the GNU General Public License as published by
+ *    the Free Software Foundation; either version 2 of the License, or
+ *    (at your option) any later version.
+ *
+ *    This program is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *    GNU General Public License for more details.
+ *
+ *    You should have received a copy of the GNU General Public License
+ *    along with this program; if not, write to the Free Software
+ *    Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+ */
+
+/*
+ *    LabelBasedFMeasure.java
+ *    Copyright (C) 2009-2010 Aristotle University of Thessaloniki, Thessaloniki, Greece
+ */
+package mulan.evaluation.measure;
+
+import java.util.ArrayList;
+import mulan.core.MulanRuntimeException;
+import weka.classifiers.evaluation.NominalPrediction;
+import weka.core.FastVector;
+
+/**
+ * Implementation of the label-based macro precision measure.
+ * 
+ * @author Grigorios Tso
+ * 
+ */
+public abstract class LabelBasedAUC extends ConfidenceMeasureBase {
+
+    protected int numOfLabels;
+    protected FastVector[] m_Predictions;
+    protected FastVector all_Predictions;
+
+    public LabelBasedAUC(int numOfLabels) {
+        this.numOfLabels = numOfLabels;
+        m_Predictions = new FastVector[numOfLabels];
+        for (int labelIndex = 0; labelIndex < numOfLabels; labelIndex++) {
+            m_Predictions[labelIndex] = new FastVector();
+        }
+        all_Predictions = new FastVector();
+    }
+
+    public void reset() {
+        for (int labelIndex = 0; labelIndex < numOfLabels; labelIndex++) {
+            m_Predictions[labelIndex] = new FastVector();
+        }
+        all_Predictions = new FastVector();
+    }
+
+    public double getIdealValue() {
+        return 1;
+    }
+
+    public double updateInternal2(double[] confidences, boolean[] truth) {
+        for (int labelIndex = 0; labelIndex < numOfLabels; labelIndex++) {
+
+            int classValue;
+            boolean actual = truth[labelIndex];
+            if (actual) {
+                classValue = 1;
+            } else {
+                classValue = 0;
+            }
+
+            double[] dist = new double[2];
+            dist[1] = confidences[labelIndex];
+            dist[0] = 1 - dist[1];
+
+            m_Predictions[labelIndex].addElement(new NominalPrediction(classValue, dist, 1));
+            all_Predictions.addElement(new NominalPrediction(classValue, dist, 1));
+        }
+
+        return 0;
+    }
+}
---
+++ src/mulan/evaluation/measure/MicroAUC.java
@@ -0,0 +1,47 @@
+/*
+ *    This program is free software; you can redistribute it and/or modify
+ *    it under the terms of the GNU General Public License as published by
+ *    the Free Software Foundation; either version 2 of the License, or
+ *    (at your option) any later version.
+ *
+ *    This program is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *    GNU General Public License for more details.
+ *
+ *    You should have received a copy of the GNU General Public License
+ *    along with this program; if not, write to the Free Software
+ *    Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+ */
+
+/*
+ *    MacroFMeasure.java
+ *    Copyright (C) 2009-2010 Aristotle University of Thessaloniki, Thessaloniki, Greece
+ */
+package mulan.evaluation.measure;
+
+import weka.classifiers.evaluation.ThresholdCurve;
+import weka.core.Instances;
+
+/**
+ * Implementation of the macro-averaged f measure.
+ *
+ * @author Grigorios Tsoumakas
+ */
+public class MicroAUC extends LabelBasedAUC {
+
+    public MicroAUC(int numOfLabels) {
+        super(numOfLabels);
+    }
+
+    public String getName() {
+        return "micro-averaged AUC";
+    }
+
+    public double getValue() {
+        ThresholdCurve tc = new ThresholdCurve();
+        System.out.println(all_Predictions);
+        Instances result = tc.getCurve(all_Predictions, 1);
+        return ThresholdCurve.getROCArea(result);
+    }
+}
