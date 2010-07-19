91625f92d8f59230da5adf19d83942a30ee813d7
Jozef Vilcek <jojovilco@gmail.com>
2010-07-19 14:40:29 +0000

    Fixing Windows version of mulan batch script ... command line arguments must be explicitly extracted and passed to java program.
    git-svn-id: svn://svn.code.sf.net/p/mulan/code/branches/console-UI@547 90ebec12-01d7-49c3-8feb-d42dc2bd4ece

--- mulan/scripts/mulan.bat
+++ mulan/scripts/mulan.bat
@@ -1,3 +1,11 @@
 @echo off
 
-java -cp mulan.jar;weka.jar;commons-cli-1.2.jar; mulan.console.CommandDispatcher
+set CMD_LINE_ARGS=
+:setupArgs
+if ""%1""=="""" goto run
+set CMD_LINE_ARGS=%CMD_LINE_ARGS% %1
+shift
+goto setupArgs
+
+:run
+java -cp mulan.jar;weka.jar;commons-cli-1.2.jar; mulan.console.CommandDispatcher %CMD_LINE_ARGS%
