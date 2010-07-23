c6fafcd51fa48620161d39b4d9828b97a94cdd39
Grigorios Tsoumakas <greg@csd.auth.gr>
2010-07-23 19:28:00 +0000

    new and updated tests
    git-svn-id: svn://svn.code.sf.net/p/mulan/code/trunk@554 90ebec12-01d7-49c3-8feb-d42dc2bd4ece

--- mulan/test/mulan/classifier/transformation/ClassifierChainTest.java
+++ mulan/test/mulan/classifier/transformation/ClassifierChainTest.java
@@ -3,11 +3,11 @@ package mulan.classifier.transformation;
 import weka.classifiers.Classifier;
 import weka.classifiers.trees.J48;
 
-public class ClassifierChainsTest extends TransformationBasedMultiLabelLearnerTest {
+public class ClassifierChainTest extends TransformationBasedMultiLabelLearnerTest {
 
     @Override
     public void setUp() {
         Classifier baseClassifier = new J48();
-        learner = new ClassifierChains(baseClassifier);
+        learner = new ClassifierChain(baseClassifier);
     }
 }
