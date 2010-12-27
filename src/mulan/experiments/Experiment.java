02fa3db508ce7fd0ae6e492f492bf9dd9df4e940
Grigorios Tsoumakas <greg@csd.auth.gr>
2010-12-27 08:42:34 +0000

    method getTechnicalInformation was not required in the abstract base class
    git-svn-id: svn://svn.code.sf.net/p/mulan/code/trunk/mulan@588 90ebec12-01d7-49c3-8feb-d42dc2bd4ece

--- src/mulan/experiments/Experiment.java
+++ src/mulan/experiments/Experiment.java
@@ -1,19 +1,14 @@
 package mulan.experiments;
 
-import weka.core.TechnicalInformation;
 import weka.core.TechnicalInformationHandler;
 
 /**
- * Abstract class for all experiments
+ * Abstract base class for all experiments
  * 
- * @author Eleftherios Spyromitros-Xioufis ( espyromi@csd.auth.gr )
+ * @author Eleftherios Spyromitros-Xioufis
+ * @author Grigorios Tsoumakas
+ * @version 2010.12.27
  */
-public abstract class Experiment implements TechnicalInformationHandler{
+public abstract class Experiment implements TechnicalInformationHandler {
 
-    /**
-     * Gets the {@link TechnicalInformation} for the current Experiment.
-     *
-     * @return technical information
-     */
-    public abstract TechnicalInformation getTechnicalInformation();
 }
