d55bcc7505168d011648327ab94bf4f0bf33c3ac
Eleftherios Spyromitros-Xioufis <espyromi@csd.auth.gr>
2012-07-26 12:12:34 +0000

    Minor updates + Javadoc fixes
    git-svn-id: svn://svn.code.sf.net/p/mulan/code/trunk@662 90ebec12-01d7-49c3-8feb-d42dc2bd4ece

--- mulan/src/mulan/evaluation/Evaluator.java
+++ mulan/src/mulan/evaluation/Evaluator.java
@@ -45,246 +45,253 @@ import weka.core.Instances;
  */
 public class Evaluator {
 
-	// seed for reproduction of cross-validation results
-	private int seed = 1;
+    // seed for reproduction of cross-validation results
+    private int seed = 1;
+    // when false divisions-by-zero are ignored in certain measures
+    private boolean strict = true;
 
-	/**
-	 * Sets the seed for reproduction of cross-validation results
-	 * 
-	 * @param aSeed seed for reproduction of cross-validation results
-	 */
-	public void setSeed(int aSeed) {
-		seed = aSeed;
-	}
+    /**
+     * Sets the seed for reproduction of cross-validation results
+     * 
+     * @param aSeed seed for reproduction of cross-validation results
+     */
+    public void setSeed(int aSeed) {
+        seed = aSeed;
+    }
 
-	/**
-	 * Evaluates a {@link MultiLabelLearner} on given test data set using
-	 * specified evaluation measures
-	 * 
-	 * @param learner the learner to be evaluated via cross-validation
-	 * @param data the data set for cross-validation
-	 * @param measures the evaluation measures to compute
-	 * @return an Evaluation object
-	 * @throws IllegalArgumentException if an input parameter is null
-	 * @throws Exception
-	 */
-	public Evaluation evaluate(MultiLabelLearner learner, MultiLabelInstances data, List<Measure> measures) throws IllegalArgumentException, Exception {
-		checkLearner(learner);
-		checkData(data);
-		checkMeasures(measures);
+    /**
+     * Controls how divisions-by-zero are handled
+     * 
+     * @param isStrict when false divisions-by-zero are ignored
+     */
+    public void setStrict(boolean isStrict) {
+        strict = isStrict;
+    }
 
-		// reset measures
-		for (Measure m : measures) {
-			m.reset();
-		}
+    /**
+     * Evaluates a {@link MultiLabelLearner} on given test data set using specified evaluation measures
+     * 
+     * @param learner the learner to be evaluated via cross-validation
+     * @param data the data set for cross-validation
+     * @param measures the evaluation measures to compute
+     * @return an Evaluation object
+     * @throws IllegalArgumentException if an input parameter is null
+     * @throws Exception
+     */
+    public Evaluation evaluate(MultiLabelLearner learner, MultiLabelInstances data, List<Measure> measures) throws IllegalArgumentException, Exception {
+        checkLearner(learner);
+        checkData(data);
+        checkMeasures(measures);
 
-		int numLabels = data.getNumLabels();
-		int[] labelIndices = data.getLabelIndices();
-		boolean[] trueLabels;
-		Set<Measure> failed = new HashSet<Measure>();
-		Instances testData = data.getDataSet();
-		int numInstances = testData.numInstances();
-		for (int instanceIndex = 0; instanceIndex < numInstances; instanceIndex++) {
-			Instance instance = testData.instance(instanceIndex);
-			if (data.hasMissingLabels(instance)) {
-				continue;
-			}
-			Instance labelsMissing = (Instance) instance.copy();
-			labelsMissing.setDataset(instance.dataset());
-			for (int i = 0; i < data.getNumLabels(); i++) {
-				labelsMissing.setMissing(data.getLabelIndices()[i]);
-			}
-			MultiLabelOutput output = learner.makePrediction(labelsMissing);
-			trueLabels = getTrueLabels(instance, numLabels, labelIndices);
-			Iterator<Measure> it = measures.iterator();
-			while (it.hasNext()) {
-				Measure m = it.next();
-				if (!failed.contains(m)) {
-					try {
-						m.update(output, trueLabels);
-					} catch (Exception ex) {
-						failed.add(m);
-					}
-				}
-			}
-		}
+        // reset measures
+        for (Measure m : measures) {
+            m.reset();
+        }
 
-		return new Evaluation(measures);
-	}
+        int numLabels = data.getNumLabels();
+        int[] labelIndices = data.getLabelIndices();
+        boolean[] trueLabels;
+        Set<Measure> failed = new HashSet<Measure>();
+        Instances testData = data.getDataSet();
+        int numInstances = testData.numInstances();
+        for (int instanceIndex = 0; instanceIndex < numInstances; instanceIndex++) {
+            Instance instance = testData.instance(instanceIndex);
+            if (data.hasMissingLabels(instance)) {
+                continue;
+            }
+            Instance labelsMissing = (Instance) instance.copy();
+            labelsMissing.setDataset(instance.dataset());
+            for (int i = 0; i < data.getNumLabels(); i++) {
+                labelsMissing.setMissing(data.getLabelIndices()[i]);
+            }
+            MultiLabelOutput output = learner.makePrediction(labelsMissing);
+            trueLabels = getTrueLabels(instance, numLabels, labelIndices);
+            Iterator<Measure> it = measures.iterator();
+            while (it.hasNext()) {
+                Measure m = it.next();
+                if (!failed.contains(m)) {
+                    try {
+                        m.update(output, trueLabels);
+                    } catch (Exception ex) {
+                        failed.add(m);
+                    }
+                }
+            }
+        }
 
-	private void checkLearner(MultiLabelLearner learner) {
-		if (learner == null) {
-			throw new IllegalArgumentException("Learner to be evaluated is null.");
-		}
-	}
+        return new Evaluation(measures);
+    }
 
-	private void checkData(MultiLabelInstances data) {
-		if (data == null) {
-			throw new IllegalArgumentException("Evaluation data object is null.");
-		}
-	}
+    private void checkLearner(MultiLabelLearner learner) {
+        if (learner == null) {
+            throw new IllegalArgumentException("Learner to be evaluated is null.");
+        }
+    }
 
-	private void checkMeasures(List<Measure> measures) {
-		if (measures == null) {
-			throw new IllegalArgumentException("List of evaluation measures to compute is null.");
-		}
-	}
+    private void checkData(MultiLabelInstances data) {
+        if (data == null) {
+            throw new IllegalArgumentException("Evaluation data object is null.");
+        }
+    }
 
-	private void checkFolds(int someFolds) {
-		if (someFolds < 2) {
-			throw new IllegalArgumentException("Number of folds must be at least two or higher.");
-		}
-	}
+    private void checkMeasures(List<Measure> measures) {
+        if (measures == null) {
+            throw new IllegalArgumentException("List of evaluation measures to compute is null.");
+        }
+    }
 
-	/**
-	 * Evaluates a {@link MultiLabelLearner} on given test data set.
-	 * 
-	 * @param learner the learner to be evaluated
-	 * @param data the data set for evaluation
-	 * @return the evaluation result
-	 * @throws IllegalArgumentException if either of input parameters is null.
-	 * @throws Exception
-	 */
-	public Evaluation evaluate(MultiLabelLearner learner, MultiLabelInstances data) throws IllegalArgumentException, Exception {
-		checkLearner(learner);
-		checkData(data);
+    private void checkFolds(int someFolds) {
+        if (someFolds < 2) {
+            throw new IllegalArgumentException("Number of folds must be at least two or higher.");
+        }
+    }
 
-		List<Measure> measures = prepareMeasures(learner, data);
+    /**
+     * Evaluates a {@link MultiLabelLearner} on given test data set.
+     * 
+     * @param learner the learner to be evaluated
+     * @param data the data set for evaluation
+     * @return the evaluation result
+     * @throws IllegalArgumentException if either of input parameters is null.
+     * @throws Exception
+     */
+    public Evaluation evaluate(MultiLabelLearner learner, MultiLabelInstances data) throws IllegalArgumentException, Exception {
+        checkLearner(learner);
+        checkData(data);
 
-		return evaluate(learner, data, measures);
-	}
+        List<Measure> measures = prepareMeasures(learner, data);
 
-	private List<Measure> prepareMeasures(MultiLabelLearner learner, MultiLabelInstances data) {
-		List<Measure> measures = new ArrayList<Measure>();
+        return evaluate(learner, data, measures);
+    }
 
-		MultiLabelOutput prediction;
-		try {
-			MultiLabelLearner copyOfLearner = learner.makeCopy();
-			prediction = copyOfLearner.makePrediction(data.getDataSet().instance(0));
-			// add bipartition-based measures if applicable
-			if (prediction.hasBipartition()) {
-				// add example-based measures
-				measures.add(new HammingLoss());
-				measures.add(new SubsetAccuracy());
-				measures.add(new ExampleBasedPrecision());
-				measures.add(new ExampleBasedRecall());
-				measures.add(new ExampleBasedFMeasure());
-				measures.add(new ExampleBasedAccuracy());
-				measures.add(new ExampleBasedSpecificity());
-				// add label-based measures
-				int numOfLabels = data.getNumLabels();
-				measures.add(new MicroPrecision(numOfLabels));
-				measures.add(new MicroRecall(numOfLabels));
-				measures.add(new MicroFMeasure(numOfLabels));
-				measures.add(new MicroSpecificity(numOfLabels));
-				measures.add(new MacroPrecision(numOfLabels));
-				measures.add(new MacroRecall(numOfLabels));
-				measures.add(new MacroFMeasure(numOfLabels));
-				measures.add(new MacroSpecificity(numOfLabels));
-			}
-			// add ranking-based measures if applicable
-			if (prediction.hasRanking()) {
-				// add ranking based measures
-				measures.add(new AveragePrecision());
-				measures.add(new Coverage());
-				measures.add(new OneError());
-				measures.add(new IsError());
-				measures.add(new ErrorSetSize());
-				measures.add(new RankingLoss());
-			}
-			// add confidence measures if applicable
-			if (prediction.hasConfidences()) {
-				int numOfLabels = data.getNumLabels();
-				measures.add(new MeanAveragePrecision(numOfLabels));
-				measures.add(new GeometricMeanAveragePrecision(numOfLabels));
-				measures.add(new MeanAverageInterpolatedPrecision(numOfLabels, 10));
-				measures.add(new GeometricMeanAverageInterpolatedPrecision(numOfLabels, 10));
-				measures.add(new MicroAUC(numOfLabels));
-				measures.add(new MacroAUC(numOfLabels));
-			}
-			// add hierarchical measures if applicable
-			if (data.getLabelsMetaData().isHierarchy()) {
-				measures.add(new HierarchicalLoss(data));
-			}
-		} catch (Exception ex) {
-			Logger.getLogger(Evaluator.class.getName()).log(Level.SEVERE, null, ex);
-		}
+    private List<Measure> prepareMeasures(MultiLabelLearner learner, MultiLabelInstances data) {
+        List<Measure> measures = new ArrayList<Measure>();
 
-		return measures;
-	}
+        MultiLabelOutput prediction;
+        try {
+            MultiLabelLearner copyOfLearner = learner.makeCopy();
+            prediction = copyOfLearner.makePrediction(data.getDataSet().instance(0));
+            // add bipartition-based measures if applicable
+            if (prediction.hasBipartition()) {
+                // add example-based measures
+                measures.add(new HammingLoss());
+                measures.add(new SubsetAccuracy());
+                measures.add(new ExampleBasedPrecision());
+                measures.add(new ExampleBasedRecall());
+                measures.add(new ExampleBasedFMeasure());
+                measures.add(new ExampleBasedAccuracy());
+                measures.add(new ExampleBasedSpecificity());
+                // add label-based measures
+                int numOfLabels = data.getNumLabels();
+                measures.add(new MicroPrecision(numOfLabels));
+                measures.add(new MicroRecall(numOfLabels));
+                measures.add(new MicroFMeasure(numOfLabels));
+                measures.add(new MicroSpecificity(numOfLabels));
+                measures.add(new MacroPrecision(numOfLabels));
+                measures.add(new MacroRecall(numOfLabels));
+                measures.add(new MacroFMeasure(numOfLabels));
+                measures.add(new MacroSpecificity(numOfLabels));
+            }
+            // add ranking-based measures if applicable
+            if (prediction.hasRanking()) {
+                // add ranking based measures
+                measures.add(new AveragePrecision());
+                measures.add(new Coverage());
+                measures.add(new OneError());
+                measures.add(new IsError());
+                measures.add(new ErrorSetSize());
+                measures.add(new RankingLoss());
+            }
+            // add confidence measures if applicable
+            if (prediction.hasConfidences()) {
+                int numOfLabels = data.getNumLabels();
+                measures.add(new MeanAveragePrecision(numOfLabels));
+                measures.add(new GeometricMeanAveragePrecision(numOfLabels));
+                measures.add(new MeanAverageInterpolatedPrecision(numOfLabels, 10));
+                measures.add(new GeometricMeanAverageInterpolatedPrecision(numOfLabels, 10));
+                measures.add(new MicroAUC(numOfLabels));
+                measures.add(new MacroAUC(numOfLabels));
+            }
+            // add hierarchical measures if applicable
+            if (data.getLabelsMetaData().isHierarchy()) {
+                measures.add(new HierarchicalLoss(data));
+            }
+        } catch (Exception ex) {
+            Logger.getLogger(Evaluator.class.getName()).log(Level.SEVERE, null, ex);
+        }
 
-	private boolean[] getTrueLabels(Instance instance, int numLabels, int[] labelIndices) {
+        return measures;
+    }
 
-		boolean[] trueLabels = new boolean[numLabels];
-		for (int counter = 0; counter < numLabels; counter++) {
-			int classIdx = labelIndices[counter];
-			String classValue = instance.attribute(classIdx).value((int) instance.value(classIdx));
-			trueLabels[counter] = classValue.equals("1");
-		}
+    private boolean[] getTrueLabels(Instance instance, int numLabels, int[] labelIndices) {
 
-		return trueLabels;
-	}
+        boolean[] trueLabels = new boolean[numLabels];
+        for (int counter = 0; counter < numLabels; counter++) {
+            int classIdx = labelIndices[counter];
+            String classValue = instance.attribute(classIdx).value((int) instance.value(classIdx));
+            trueLabels[counter] = classValue.equals("1");
+        }
 
-	/**
-	 * Evaluates a {@link MultiLabelLearner} via cross-validation on given data
-	 * set with defined number of folds and seed.
-	 * 
-	 * @param learner the learner to be evaluated via cross-validation
-	 * @param data the multi-label data set for cross-validation
-	 * @param someFolds
-	 * @return a {@link MultipleEvaluation} object holding the results
-	 */
-	public MultipleEvaluation crossValidate(MultiLabelLearner learner, MultiLabelInstances data, int someFolds) {
-		checkLearner(learner);
-		checkData(data);
-		checkFolds(someFolds);
+        return trueLabels;
+    }
 
-		return innerCrossValidate(learner, data, false, null, someFolds);
-	}
+    /**
+     * Evaluates a {@link MultiLabelLearner} via cross-validation on given data set with defined number of folds and seed.
+     * 
+     * @param learner the learner to be evaluated via cross-validation
+     * @param data the multi-label data set for cross-validation
+     * @param someFolds
+     * @return a {@link MultipleEvaluation} object holding the results
+     */
+    public MultipleEvaluation crossValidate(MultiLabelLearner learner, MultiLabelInstances data, int someFolds) {
+        checkLearner(learner);
+        checkData(data);
+        checkFolds(someFolds);
 
-	/**
-	 * Evaluates a {@link MultiLabelLearner} via cross-validation on given data
-	 * set using given evaluation measures with defined number of folds and
-	 * seed.
-	 * 
-	 * @param learner the learner to be evaluated via cross-validation
-	 * @param data the multi-label data set for cross-validation
-	 * @param measures the evaluation measures to compute
-	 * @param someFolds
-	 * @return a {@link MultipleEvaluation} object holding the results
-	 */
-	public MultipleEvaluation crossValidate(MultiLabelLearner learner, MultiLabelInstances data, List<Measure> measures, int someFolds) {
-		checkLearner(learner);
-		checkData(data);
-		checkMeasures(measures);
+        return innerCrossValidate(learner, data, false, null, someFolds);
+    }
 
-		return innerCrossValidate(learner, data, true, measures, someFolds);
-	}
+    /**
+     * Evaluates a {@link MultiLabelLearner} via cross-validation on given data set using given evaluation measures with defined number of folds and seed.
+     * 
+     * @param learner the learner to be evaluated via cross-validation
+     * @param data the multi-label data set for cross-validation
+     * @param measures the evaluation measures to compute
+     * @param someFolds
+     * @return a {@link MultipleEvaluation} object holding the results
+     */
+    public MultipleEvaluation crossValidate(MultiLabelLearner learner, MultiLabelInstances data, List<Measure> measures, int someFolds) {
+        checkLearner(learner);
+        checkData(data);
+        checkMeasures(measures);
 
-	private MultipleEvaluation innerCrossValidate(MultiLabelLearner learner, MultiLabelInstances data, boolean hasMeasures, List<Measure> measures, int someFolds) {
-		Evaluation[] evaluation = new Evaluation[someFolds];
+        return innerCrossValidate(learner, data, true, measures, someFolds);
+    }
 
-		Instances workingSet = new Instances(data.getDataSet());
-		workingSet.randomize(new Random(seed));
-		for (int i = 0; i < someFolds; i++) {
-			System.out.println("Fold " + (i + 1) + "/" + someFolds);
-			try {
-				Instances train = workingSet.trainCV(someFolds, i);
-				Instances test = workingSet.testCV(someFolds, i);
-				MultiLabelInstances mlTrain = new MultiLabelInstances(train, data.getLabelsMetaData());
-				MultiLabelInstances mlTest = new MultiLabelInstances(test, data.getLabelsMetaData());
-				MultiLabelLearner clone = learner.makeCopy();
-				clone.build(mlTrain);
-				if (hasMeasures)
-					evaluation[i] = evaluate(clone, mlTest, measures);
-				else
-					evaluation[i] = evaluate(clone, mlTest);
-			} catch (Exception ex) {
-				Logger.getLogger(Evaluator.class.getName()).log(Level.SEVERE, null, ex);
-			}
-		}
-		MultipleEvaluation me = new MultipleEvaluation(evaluation);
-		me.calculateStatistics();
-		return me;
-	}
+    private MultipleEvaluation innerCrossValidate(MultiLabelLearner learner, MultiLabelInstances data, boolean hasMeasures, List<Measure> measures, int someFolds) {
+        Evaluation[] evaluation = new Evaluation[someFolds];
+
+        Instances workingSet = new Instances(data.getDataSet());
+        workingSet.randomize(new Random(seed));
+        for (int i = 0; i < someFolds; i++) {
+            System.out.println("Fold " + (i + 1) + "/" + someFolds);
+            try {
+                Instances train = workingSet.trainCV(someFolds, i);
+                Instances test = workingSet.testCV(someFolds, i);
+                MultiLabelInstances mlTrain = new MultiLabelInstances(train, data.getLabelsMetaData());
+                MultiLabelInstances mlTest = new MultiLabelInstances(test, data.getLabelsMetaData());
+                MultiLabelLearner clone = learner.makeCopy();
+                clone.build(mlTrain);
+                if (hasMeasures)
+                    evaluation[i] = evaluate(clone, mlTest, measures);
+                else
+                    evaluation[i] = evaluate(clone, mlTest);
+            } catch (Exception ex) {
+                Logger.getLogger(Evaluator.class.getName()).log(Level.SEVERE, null, ex);
+            }
+        }
+        MultipleEvaluation me = new MultipleEvaluation(evaluation);
+        me.calculateStatistics();
+        return me;
+    }
 }
